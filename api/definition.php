e68bca8ab99b31a9c6c956740756bb8c9b8f92a5
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-22 13:41:54 +0000

    new

---
+++ api/definition.php
@@ -0,0 +1,35 @@
+<?php
+
+require "../classes/rest.php";
+require "../classes/definition.php";
+
+$data = RestUtils::processRequest();  
+
+switch($data->getMethod()) 
+{  
+    // this is a request for all users, not one in particular  
+    case 'get':  
+        $definition = new Definition();
+        $definition->setName = "TestName";
+        $definition->setDescription = "TestDesc";
+        if($data->getHttpAccept == 'json')  
+        {
+            RestUtils::sendResponse(200, json_encode($array), 'application/json');  
+        }
+    
+        else if ($data->getHttpAccept == 'xml')  
+        {
+            // using the XML_SERIALIZER Pear Package  
+            $options = array  
+            (  
+                'indent' => '     ',  
+                'addDecl' => false,  
+                'rootName' => $fc->getAction(),  
+                XML_SERIALIZER_OPTION_RETURN_RESULT => true  
+            );  
+            $serializer = new XML_Serializer($options);  
+            RestUtils::sendResponse(200, $serializer->serialize($array), 'application/xml');  
+        }  
+        break;
+}
+?>
--- classes/definition.php
+++ classes/definition.php
@@ -1,48 +1,37 @@
 <?php
-/*------------------------------------------------------------------------------
-** File:        task.php
-** Description: Handles the task 
-** Version:     1.0
-** Author:      Mattias Wolff
-** Email:       mattias dot wolff at gmail dot com
-** Homepage:    www.brenorbrophy.com 
-*/
 
 class Definition {
-  public $name;
-  public $description;
-  public $info 	= array();
-  
-  public function insert () {
-    require ($_SERVER["DOCUMENT_ROOT"]."/scripts/connectMongoDb.php");   
-	$db->definitions->insert($this);
-  }
-  
-  public function update () {
-    require ($_SERVER["DOCUMENT_ROOT"]."/scripts/connectMongoDb.php");
-    echo "obj " . $this->_id;
-	$result = $db->command(array('findAndModify' => 'definitions', 
-	'query' => array('_id' => new MongoId($this->_id)),
-    'update' => array('name' => $this->name, 'description' => $this->description, 'info' => $this->info),
-    'new' => true,        # To get back the document after the upsert
-    'upsert' => true,
-    'fields' => array( '_id' => 1 )   # Only return _id field
-) );
 
-	$this->_id = $result['value']['_id'];
-  }
+  //Properties
+  private $name;
+  private $description;
+  private $content;
   
-  public function get ($id) {
-    require ($_SERVER["DOCUMENT_ROOT"]."/scripts/connectMongoDb.php");
-	$results = $db->definitions->findOne(array('_id' => new MongoId($id)));
-	$this->name = $results['name'];
-	$this->description = $results['description'];
-	$this->info = $results['info'];
-  }
-  
-  public function delete ($id) {
-    require ($_SERVER["DOCUMENT_ROOT"]."/scripts/connectMongoDb.php");
-	$results = $db->definitions->delete(array('_id' => new MongoId($id)));
-  }
+  //Constructor
+  public function __construct(){  
+    $this->name = '';  
+    $this->description = '';  
+    $this->content = array();    
+    }
+    
+  //Accessors
+  public function getName() {
+    return $this->name;
+    }
+  public function setName($name) {
+    $this->name = $name;
+    }
+  public function getDescription() {
+    return $this->description;
+    }
+  public function setDescription($description) {
+    $this->description = $description;
+    }
+  public function getContent() {
+    return $this->name;
+    }
+  public function setContent($content) {
+    $this->content = $content;
+    }
 }
 ?>
