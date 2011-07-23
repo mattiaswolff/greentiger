ec50316c22d78763ff9b3377a9b3f6c0df66a779
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-23 10:03:18 +0000

    user

--- client/user.php
+++ client/user.php
@@ -9,7 +9,7 @@
 
 <body id="home">
 
-				<form name="register" action="../api/user.php" method="post">
+				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="post">
    		 			Email: <input type="text" name="userId" maxlength="30" />
     					Password: <input type="password" name="pass1" />
     					Password Again: <input type="password" name="pass2" />
