8a6b8c7ac8ca7c5e10ff3e1ee359dd15933c8948
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-23 16:15:04 +0000

    user

--- api/user.php
+++ api/user.php
@@ -29,7 +29,7 @@ switch($data->getMethod()) {
         break;
     case 'post':
         $user = new User();
-        //$user->setEmail($_POST('email'));
+        $user->setEmail('test');
         //$user->setName($_POST('name'));
         echo 'good!';
         break;
