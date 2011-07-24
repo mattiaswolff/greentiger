8dbc9f28158321f47adbef9ef49fc6a4643a15fc
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-24 17:47:19 +0000

    user

--- client/user.php
+++ client/user.php
@@ -8,12 +8,14 @@
 </head>
 
 <body id="home">
+                
                 Post
 				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="post">
    		 			    Email: <input type="text" name="email" maxlength="30" />
     					Name: <input type="text" name="name" />
     					<input type="submit" value="Register" />
 				</form>
+                
                 Put
     			<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="put">
    		 			    Email: <input type="text" name="email" maxlength="30" />
