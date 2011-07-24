1cc841b3dae0d31444e211bed29aeebda8fba770
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-24 15:22:35 +0000

    user

--- classes/user.php
+++ classes/user.php
@@ -32,7 +32,7 @@ class User {
         return $this->email;
     }
     public function setEmail($email) {
-        if (preg_match('^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$', $email)) {
+        if (preg_match("/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)) {
             $this->email = (string)$email;
         }
         else {
