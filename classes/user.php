581bef10e3982489cbc339b6c2077f48ecffff4d
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-24 14:51:11 +0000

    user

--- classes/user.php
+++ classes/user.php
@@ -32,7 +32,8 @@ class User {
         return $this->email;
     }
     public function setEmail($email) {
-        if preg_match("\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", $email) > 0 {
+        $strRegex = "\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b";
+        if preg_match($strRegex, $email) > 0 {
             $this->email = $email;
         }
         else {
