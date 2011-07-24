68e2a436b67a5e89f1afdd7162cfb21067a01f4f
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-24 21:50:48 +0000

    user

--- classes/rest.php
+++ classes/rest.php
@@ -20,12 +20,12 @@ class RestUtils
                 // and then parse it out into an array via parse_str... per the PHP docs:  
                 // Parses str as if it were the query string passed via a URL and sets  
                 // variables in the current scope.  
-                parse_str(file_get_contents('php://input'), $put_vars);  
-                $data = $put_vars;  
+                parse_str(file_get_contents('php://input'), $arrVars);  
+                $data = $arrVars;  
                 break;
             case 'delete':    
-                parse_str(file_get_contents('php://input'), $put_vars);  
-                $data = $put_vars;  
+                parse_str(file_get_contents('php://input'), $arrVars);  
+                $data = $arrVars;  
                 break;
         }  
   
