541d18f7f12436e91e1643e08195775291c75d04
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-25 13:32:51 +0000

    user

---
+++ client/definition.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+</head>
+
+<body id="home">
+                test
+                Post
+				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=mattias.wolff@gmail.com" method="post">
+   		 			    Name: <input type="text" name="name" maxlength="30" />
+    					Desc: <input type="text" name="description" />
+    					<input type="submit" value="Register" />
+				</form>
+
+</body>
+</html>      
