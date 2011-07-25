d6baa0580efccef26795ae442b7fcb0c7ac17927
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-25 15:20:14 +0000

    user

--- client/definition.php
+++ client/definition.php
@@ -10,7 +10,7 @@
 <body id="home">
                 test
                 Post
-				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=mattias.wolff@gmail.com" method="post">
+				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=matwo065" method="post">
    		 			    Name: <input type="text" name="name" maxlength="30" />
     					Desc: <input type="text" name="description" />
     					<input type="submit" value="Register" />
