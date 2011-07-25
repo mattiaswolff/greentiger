184f8462d8e9e63b5b5541db9a71ee0a52135b76
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-25 07:53:24 +0000

    user

--- classes/rest.php
+++ classes/rest.php
@@ -69,19 +69,7 @@ class RestUtils
             $signature = ($_SERVER['SERVER_SIGNATURE'] == '') ? $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT'] : $_SERVER['SERVER_SIGNATURE'];  
   
             // this should be templatized in a real-world solution  
-            $body = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">  
-                        <html>  
-                            <head>  
-                                <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">  
-                                <title>' . $status . ' ' . RestUtils::getStatusCodeMessage($status) . '</title>  
-                            </head>  
-                            <body>  
-                                <h1>' . RestUtils::getStatusCodeMessage($status) . '</h1>  
-                                <p>' . $message . '</p>  
-                                <hr />  
-                                <address>' . $signature . '</address>  
-                            </body>  
-                        </html>';  
+            $body = 'test';  
             
             echo $body;  
             exit;  
