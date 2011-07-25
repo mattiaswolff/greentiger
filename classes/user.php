ed9076154b89277b42e2c2b49bcf22d701246a22
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-25 09:21:10 +0000

    user

--- classes/user.php
+++ classes/user.php
@@ -82,7 +82,7 @@ class User {
         $db = $m->projectcopperfield;
 	    $arrQuery = array("email" => $strEmail);
 	    $bolSuccess = $db->users->remove($arrQuery);
-        if $bolSuccess ? echo 'yes' : 'no';
+        if ($bolSuccess ? echo 'yes' : 'no');
     }
 }
 ?>
