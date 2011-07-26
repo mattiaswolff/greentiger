519f7520cd2fbc3c69633ea0ba461115c29fd85d
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-26 11:30:21 +0000

    user

--- api/user.php
+++ api/user.php
@@ -16,7 +16,7 @@ switch($data->getMethod()) {
         $arrRequestVars = $data->getRequestVars();
         if (isset($arrRequestVars['userId'])) {
             $user = new User();
-            $user->setId($arrRequestVars["userId"]);
+            $user->setId(new MongoId());
             $user->setEmail($arrRequestVars["email"]);
             $user->setName($arrRequestVars["name"]);
             $user->upsert();
