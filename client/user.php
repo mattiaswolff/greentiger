c2f77c89107895d18bb555432d12fe32e55120a0
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-27 20:26:44 +0000

    user

--- client/user.php
+++ client/user.php
@@ -9,32 +9,18 @@
         $(document).ready(function(){
     	    $.getJSON("http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php", { userId: <?php echo '"'. $_GET['userId'] .'"' ?> }, function(json) {
                 $("#name").val(json.users[0].name);
+                $("#email").val(json.users[0].email);
+                $("#userId").val(json.users[0]._id);
             });
 	    });
     </script>
 </head>
 
 <body id="home">
-                test
-                Post
-				<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="post">
+				<form>
    		 			    UserId: <input id="userId" type="text" name="userId" />
+                        Name: <input id="name" type="text" name="name" />
                         Email: <input id="email" type="text" name="email" maxlength="30" />
-    					Name: <input id="name" type="text" name="name" />
-    					<input type="submit" value="Register" />
-				</form>
-                
-                Put
-    			<form name="register" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="put">
-   		 			    Email: <input type="text" name="emailput" maxlength="30" />
-    					Name: <input type="text" name="nameput" />
-    					<input type="submit" value="Update" />
-				</form>
-                
-                Delete
-                <form name="delete" action="http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php" method="delete">
-   		 			    Email: <input type="text" name="emaildelete" maxlength="30" />
-    					<input type="submit" value="delete" />
 				</form>
 </body>
 </html>      
