11f69efea574c96c525d52843a242141dd870b3f
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-27 17:44:31 +0000

    user

--- client/definition.php
+++ client/definition.php
@@ -26,7 +26,7 @@
     function send(){
 		$.ajax({
         type: "POST",
-        url: "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId="<?php $_GET['userId']?>,
+        url: "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=<?php $_GET['userId']?>",
         dataType: 'json',
         data: {'name': 'testdesc', 'description': 'testdescription', 'content' : { '0' : {'name' : 'testName', 'description' : 'TestDescr', 'type' : 'text'}}},
         success: function(msg){
