7b62bb158729339a3b0e0d33cb8f162a7b30fdd0
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-29 18:18:57 +0000

    user

---
+++ js/jquery.greentiger.js
@@ -0,0 +1,25 @@
+(function submitFormJSON(strURL, strType) {
+            var objFormValues = {};
+            $.each($('form').serializeArray(), function(key,value) {
+                objFormValues[value.name] = value.value;
+            });
+            $.ajax({
+                type: strType,
+                url: strURL,
+                dataType: 'json',
+                data: objFormValues,
+                success: function(msg) {
+                    alert( "Data Saved!");
+                }
+            });
+        }
+        function getParameterByName(name) {
+            name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+            var regexS = "[\\?&]" + name + "=([^&#]*)";
+            var regex = new RegExp(regexS);
+            var results = regex.exec(window.location.href);
+            if(results == null)
+                return "";
+            else
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        })
