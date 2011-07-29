4618174910d5aa12d4f30920347df7fea1b55f9a
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-29 13:18:28 +0000

    user

--- client/user.php
+++ client/user.php
@@ -6,8 +6,8 @@
     <script type="text/javascript" src="../js/jquery.js"></script>
     <script type="text/javascript">	
         $(document).ready(function(){
-            var userId = getParameterByName("userId"); 
-            if (getParameterByName("userId") != "" ) {
+            var strUserId = getParameterByName("userId"); 
+            if (userId != "" ) {
                 $.getJSON("http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php", { userId: <?php echo (isset($_GET['userId']) ? '"'. $_GET['userId'] .'"' : '""' ) ?> }, function(json) {
                     $("#name").val(json.users[0].name);
                     $("#email").val(json.users[0].email);
