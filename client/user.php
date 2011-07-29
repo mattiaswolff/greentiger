762e7cbc0dfe7ac6b01d29865f1b2e0311b51e0a
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-29 18:48:46 +0000

    user

--- client/user.php
+++ client/user.php
@@ -50,9 +50,9 @@
                         Name: <input id="name" type="text" name="name" value="" /><br/>
                         Email: <input id="email" type="text" name="email" maxlength="30" value="" /><br/>
 				</form>
-                <span onClick=<?php echo "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php', 'POST')"; ?>>Save user</span>
+                <span onClick=<?php echo """submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/user.php', 'POST')"""; ?>>Save user</span>
     <section>
-        Definitions
+        Definitions12
     			<form id="definitions">
 				</form>
                 <span onClick="submitDefinitions()">Save definitions</span>
