25181b0d0f7e67cb808bc93a9dc79abb6233c425
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-29 19:42:17 +0000

    user

--- client/definition.php
+++ client/definition.php
@@ -28,7 +28,7 @@
     
     function addFormRow(){
     	var newrow = document.createElement('article');
-		newrow.innerHTML = 'Name: <input type="text" name="name" value="" /> Description: <input type="text" /> Type: <select class="field"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select>';
+		newrow.innerHTML = 'Name: <input type="text" name="array[''content''][''name'']" value="" /> Description: <input type="text" name="array[''content''][''description'']" value="" /> Type: <select class="field"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select>';
 		document.getElementById("section").appendChild(newrow);
     }
     
