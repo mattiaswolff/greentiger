bad685ccf91bce25eef7bac41ea1b127af235faa
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-29 22:51:54 +0000

    user

--- client/definition.php
+++ client/definition.php
@@ -9,24 +9,6 @@
     <script type="text/javascript" src="../js/jquery.toObject.js"></script>
     <script type="text/javascript" src="../js/form2object.js"></script>
 	<script type="text/javascript">	
-    $(document).ready(function(){
-        
-    	$('td.delete > a').click(function(event){
-			$(this).parent().parent().remove();
-		});
-	});
-    
-    function send(){
-		$.ajax({
-        type: "POST",
-        url: <?php echo '"http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=' . $_GET["userId"] . '"'?>,
-        dataType: 'json',
-        data: {'name': 'testdesc', 'description': 'testdescription', 'content' : { '0' : {'name' : 'testName', 'description' : 'TestDescr', 'type' : 'text'}}},
-        success: function(msg){
-            alert( "Data Saved: " + msg );
-        }
-        });
-    }
     
     function addFormRow(){ 
         var newrow = document.createElement('article');
@@ -34,21 +16,6 @@
 		newrow.innerHTML = '<div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <input type="text" name="content[' + counter + '].description" value="" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select></div>';
 		document.getElementById("section").appendChild(newrow);
     }
-    
-    function showValues() {
-    var values = {};
-    $.each($('#section').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-    });
-    alert(values);
-      var str = $("section").serialize();
-      $("#results").text(str);
-    }
-    
-    $('form').submit(function() {
-  console.log($(this).serializeArray());
-  return false;
-});
     
 	</script>
 
@@ -63,13 +30,8 @@
             <input type="text" name="description" value="" />
         </form>
     </section>
-        <span onclick="addFormRow()">Add form row</span>
-        
+        <span onclick="addFormRow()">Add form row</span></br>
         <span onClick=<?php echo (!isset($_GET['definitionId']) ? '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=matwo065', 'POST')" . '"' : '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=matwo065', 'PUT')" . '"' ); ?>>Save</span>
-        
-        <span id="results"></span>
-                
-                <span onclick="send()">Send</span>
 
 </body>
 </html>      
