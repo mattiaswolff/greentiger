624053decc06eae9fd67b87573e3aaae17b2e1f0
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-30 07:30:33 +0000

    user

--- client/definition.php
+++ client/definition.php
@@ -8,7 +8,22 @@
     <script type="text/javascript" src="../js/jquery.js"></script>
     <script type="text/javascript" src="../js/jquery.toObject.js"></script>
     <script type="text/javascript" src="../js/form2object.js"></script>
-	<script type="text/javascript">	
+	<script type="text/javascript">
+        $(document).ready(function(){
+            var strUserId = getParameterByName("userId"); 
+            if (getParameterByName("definitionId") != "" ) {
+                $.getJSON("http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php", { definitionId: <?php echo (isset($_GET['definitionId']) ? '"'. $_GET['definitionId'] .'"' : '""' ) ?> }, function(json) {
+                    $("#name").val(json.users[0].name);
+                    $("#email").val(json.users[0].email);
+                    $.each(json.definitions[0].definitions, function(key, value) {
+                        var newrow = document.createElement('article');
+                        var counter = document.getElementsByClassName('formRow').length;
+    	                newrow.innerHTML = '<div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="' + value.name + '" /> Description: <input type="text" name="content[' + counter + '].description" value="' + value.type + '" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select></div>';
+		                document.getElementById("section").appendChild(newrow);
+                    });
+                });
+            }
+        });
     
     function addFormRow(){ 
         var newrow = document.createElement('article');
@@ -34,4 +49,4 @@
         <span onClick=<?php echo (!isset($_GET['definitionId']) ? '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=matwo065', 'POST')" . '"' : '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php?userId=matwo065', 'PUT')" . '"' ); ?>>Save</span>
 
 </body>
-</html>      
+</html>
