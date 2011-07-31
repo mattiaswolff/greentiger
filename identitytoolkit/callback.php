8c78b830f1ff519b8ee6b580018243ac85e7452e
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-31 21:44:03 +0000

    user

--- identitytoolkit/callback.php
+++ identitytoolkit/callback.php
@@ -4,12 +4,10 @@ require "../classes/definition.php";
 require "../classes/user.php";
 
 $data = RestUtils::processRequest();  
+$arrRequestVars = $data->getRequestVars();
 
-switch($data->getMethod()) {  
-    case 'get':
-        $arrRequestVars = $data->getRequestVars();
-        echo "get: " . var_dump($arrRequestVars) . "getEnd";
-        echo '<script type="text/javascript">
+echo "get: " . var_dump($arrRequestVars) . "getEnd";
+echo '<script type="text/javascript">alert("test");
             var objJSON = {"requestUri": "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/index.php", "postBody": $arrRequestVars}
             $.ajax({
                 type: "POST",
@@ -17,20 +15,5 @@ switch($data->getMethod()) {
                 dataType: "json",
                 data: objJSON
                 }
-            });';        
-        break;
-    case 'post':
-        $arrRequestVars = $data->getRequestVars();
-        echo "Post: " . var_dump($arrRequestVars) . "postEnd";
-        echo '<script type="text/javascript">
-            var objJSON = {"requestUri": "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/index.php", "postBody": $arrRequestVars}
-            $.ajax({
-                type: "POST",
-                url: "https://www.googleapis.com/identitytoolkit/v1/relyingparty/verifyAssertion?key=AIzaSyD_mpU7Xw4GeTmQNqHgIuZFVyPXdOyj6qY",
-                dataType: "json",
-                data: objJSON
-                }
-            });';        
-        break;
-}
+            });<script>';
 ?>
