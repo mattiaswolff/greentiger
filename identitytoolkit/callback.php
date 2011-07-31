d08c261cb2ef9fcfb1c34cdf33128f0ab150eb62
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-31 21:59:21 +0000

    user

--- identitytoolkit/callback.php
+++ identitytoolkit/callback.php
@@ -19,7 +19,5 @@ require "../classes/user.php";
 
 $data = RestUtils::processRequest();  
 $arrRequestVars = $data->getRequestVars();
-
-echo JSON_encode($arrRequestVars);
 echo '<script type="text/javascript">var obj = jQuery.parseJSON(' . JSON_encode($arrRequestVars) . ');alert("test" + obj.openid_ns)</script>';
 ?>
