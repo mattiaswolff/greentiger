d740a5c419b691d6c79906c756adcf56e6bfee84
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-31 22:18:02 +0000

    user

--- identitytoolkit/callback.php
+++ identitytoolkit/callback.php
@@ -8,16 +8,21 @@ require "../classes/user.php";
     <script type="text/javascript" src="../js/jquery.toObject.js"></script>
     <script type="text/javascript" src="../js/form2object.js"></script>
 	<script type="text/javascript">
-    function addFormRow(){ 
-        var newrow = document.createElement('article');
-        var counter = document.getElementsByClassName('formRow').length;
-		newrow.innerHTML = '<div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <input type="text" name="content[' + counter + '].description" value="" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select></div>';
-		document.getElementById("section").appendChild(newrow);
+    function postJSON(strUrl, strJson){ 
+        var objJSON = jQuery.parseJSON(strJson);
+        $.ajax({
+                type: 'POST',
+                url: strUrl,
+                dataType: 'json',
+                data: objJSON,
+                success: function(msg) {
+                    alert( "Data Saved!");
+                });
     }  
 	</script>
 <?php
 
 $data = RestUtils::processRequest();  
 $arrRequestVars = $data->getRequestVars();
-echo '<script type="text/javascript">var obj = jQuery.parseJSON('. "'" . JSON_encode($arrRequestVars) . "'" .');alert("test" + obj.openid_ns)</script>';
+echo '<script type="text/javascript">postJSON(' . "'" . JSON_encode($arrRequestVars) . "'" . ');</script>'
 ?>
