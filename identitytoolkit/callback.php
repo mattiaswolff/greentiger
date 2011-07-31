78cda774892c74998a7225a99157092a03fe29a7
Mattias Wolff <mattias.wolff@gmail.com>
2011-07-31 21:53:46 +0000

    user

--- identitytoolkit/callback.php
+++ identitytoolkit/callback.php
@@ -8,13 +8,4 @@ $arrRequestVars = $data->getRequestVars();
 
 echo JSON_encode($arrRequestVars);
 echo '<script type="text/javascript">var obj = jQuery.parseJSON(' . JSON_encode($arrRequestVars) . ');alert("test" + obj.openid_ns)</script>';
-echo '<script type="text/javascript">
-            var objJSON = {"requestUri": "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/index.php", "postBody": $arrRequestVars}
-            $.ajax({
-                type: "POST",
-                url: "https://www.googleapis.com/identitytoolkit/v1/relyingparty/verifyAssertion?key=AIzaSyD_mpU7Xw4GeTmQNqHgIuZFVyPXdOyj6qY",
-                dataType: "json",
-                data: objJSON
-                }
-            });</script>';
 ?>
