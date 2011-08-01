a786ff48e4804a5b94d8c1ea0addd10be9df7ca6
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 15:08:54 +0000

    git

--- git/auth.php
+++ git/auth.php
@@ -3,11 +3,12 @@ require "../classes/user.php";
 session_start();
 if (isset($_SESSION['userId'])) {
     $objUser = new User($_SESSION['userId']);
-    $arrAccessToken = $objUser->getAccessToken(); 
-    $arrAccessToken[] = array(rand(8,12), new MongoDate());
-    $objUser->setAccessToken($arrAccessToken);
+    $arrAccessTokens = $objUser->getAccessTokens(); 
+    $arrNewAccessToken = array(rand(8,12), new MongoDate());
+    $arrAccessTokens[] = $arrNewAccessToken;
+    $objUser->setAccessTokens($arrAccessToken);
     $objUser->upsert();
-    $strAccessToken = md5("userid:" . $_SESSION['userId'] . "accesstoken:" . $arrAccessToken[0]); 
+    $strAccessToken = md5("userid:" . $_SESSION['userId'] . "accesstoken:" . $arrNewAccessToken[0]); 
     header('Location: ' . $_SESSION['redirectUri'] . '#access_token=' . $strAccessToken . '&token_type=example&expires_in=4301');
 }
 else {
