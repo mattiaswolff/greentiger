2beca1a8f1a189eb79dc48404eb765a3c783f86b
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 12:34:17 +0000

    git

--- client/user.php
+++ client/user.php
@@ -66,7 +66,7 @@
    		 			    UserId: <input id="userId" type="text" name="userId" value=""<?php echo (isset($_GET['userId']) ? 'readonly="readonly"' : '') ?> /><br/>
                         Name: <input id="name" type="text" name="name" value="" /><br/>
                         Email: <input id="email" type="text" name="email" maxlength="30" value="" /><br/>
-                        Redirect Uri: <input id="redirectUri" type="text" name="redirectUri" maxlength="30" value="" /><br/>
+                        Redirect Uri: <input id="redirectUri" type="text" name="redirectUri" maxlength="200" value="" /><br/>
 				</form>
                 <span onClick=<?php echo (!isset($_GET['userId']) ? '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/users', 'POST')" . '"' : '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/users/" . $_GET['userId'] ."', 'PUT')" . '"' ); ?>>Save user</span>
     <section>
