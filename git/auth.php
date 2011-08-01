fb8e8e0c92f9251025e1a8bb03ecd83dfdc476c8
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 14:09:02 +0000

    user

--- git/auth.php
+++ git/auth.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-HTTP/1.1 302 Found
-     Location: http://example.com/rd#access_token=2YotnFZFEjr1zCsicMWpAA&state=xyz&token_type=example&expires_in=3600
 if (isset($_SESSION['userId'])) {
     header('Location: ' . $_SESSION['redirectUri'] . '#access_token=1/QbIbRMWW&token_type=example&expires_in=4301');
 }
