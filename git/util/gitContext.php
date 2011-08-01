7c9ffca8e7d12b3c08ae1e66c32212e1f26f7d7b
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 09:12:44 +0000

    git

--- git/util/gitContext.php
+++ git/util/gitContext.php
@@ -58,7 +58,7 @@ class gitContext {
     if (!self::$sessionManager) {
       $className = self::getConfig()->getSessionManagerName();
       if (self::loadExternal($className)) {
-        self::$sessionManager = new $className(self::getConfig());
+        self::$sessionManager = new $className(self::getConfig(), self::getAccountService());
       }
     }
     return self::$sessionManager;
