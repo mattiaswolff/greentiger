90f3cad8fbdf3f07d171569c5e5cd3582488e72b
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 20:28:23 +0000

    git

--- classes/user.php
+++ classes/user.php
@@ -120,7 +120,6 @@ class User {
         $date = new DateTime();
         $intSec = $date->getTimestamp();
         $booReturn = FALSE;
-        if
         $arrAccessTokens = array();
         foreach ($arrResults['accessTokens'] as $key => $value) {
             $objMongoDate = $value['createdDate'];
