d92ff3137a5f570a1230f7466f5b40d6fbd9f513
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 13:41:18 +0000

    user

--- classes/user.php
+++ classes/user.php
@@ -98,8 +98,7 @@ class User {
     function validateConsumer($strClientId, $strRedirectUri) {
         $m = new Mongo();
         $db = $m->projectcopperfield;
-
-        $arrResults = $db->users->findOne(array("client_id" => $strClientId, "redirect_uri" => $strRedirectUri));
+        $arrResults = $db->users->findOne(array("client_id" => new MongoId($strClientId), "redirect_uri" => $strRedirectUri));
         echo var_dump($arrResults);
         if ($arrResults != null) {
             return TRUE;
