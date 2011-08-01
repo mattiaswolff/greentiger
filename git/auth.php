da6e9fd651559aa128928850039436ff181da1a8
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-01 11:35:33 +0000

    git

---
+++ git/auth.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+if isset($_SESSION['userId']) {
+    header('Location: ' . $_SESSION['redirectUri'] . '?access_token=1/QbIbRMWW&expires_in=4301');
+    else {
+    header('Location: ' . $_SESSION['redirectUri'] . '?error=access_denied');
+    }
+?>
