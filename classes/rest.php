ce7c767b894ebc9f3f081dde88e42198d5795d2e
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-03 20:10:56 +0000

    user

--- classes/rest.php
+++ classes/rest.php
@@ -151,7 +151,7 @@ class RestUtils
             402 => 'Payment Required',  
             403 => 'Forbidden',  
             404 => 'Not Found',  
-            405 => 'Method Not Allowed',  
+            405 => 'Method Not Allowed1',  
             406 => 'Not Acceptable',  
             407 => 'Proxy Authentication Required',  
             408 => 'Request Timeout',  
