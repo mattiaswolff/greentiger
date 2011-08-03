0df9ca296fd32e1460f294daa555db7f91865dce
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-03 12:22:12 +0000

    git

--- scripts/connectMySQL.php
+++
@@ -1,8 +0,0 @@
-<?php	
-	$dbhost = 'localhost';
-	$dbname = 'projectcopperfield';
-	$dbuser = 'copperfield';
-	$dbpass = 'dMKu893jd9l'; //not really
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-	mysql_select_db($dbname, $conn);
-?>
--- showAttachment.php
+++
@@ -1,7 +0,0 @@
-<?php	
-	require ($_SERVER["DOCUMENT_ROOT"]."/scripts/connectMongoDb.php");
-	$grid = $db->getGridFS();
-	$attachment = $grid->findOne(array("_id" => new Mongoid($_GET['attachmentId'))); 
-	header('Content-type:' . $attachment->file["contentType"]);
-	echo $attachment->getBytes();
-?>
