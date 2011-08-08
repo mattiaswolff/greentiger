b71c14d5411f1e8d23105d85a34476ccfb90741e
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-08 20:52:09 +0000

    dashboard

--- client/dashboard.php
+++ client/dashboard.php
@@ -24,7 +24,7 @@
                     $.each(json.definitions, function(key, value) {
                         var counter = $('.definitions > article').length;
                         var strHtml = '<article id="' + value._id.$id +'"><span class="header">' + value.name + '(<span class="total"></span>)</span><table><thead><tr><th>Updated</th><th>Title</th><th>C</th><th>L</th></tr></thead><tbody></tbody></table><a href="">View all</a></article>';
-		                var strHtml = '<span class="button">' + value.name + '</span>';
+		                var strHtml2 = '<span class="button">' + value.name + '</span>';
                         $('.definitions').append(strHtml);
                         $('.createTask').append(strHtml2);
                     });
