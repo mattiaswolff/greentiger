a368bfdf7f6c4aa9eeee561aa302961a0ab0732d
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-15 19:51:35 +0000

    user

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -4,5 +4,5 @@
     <input type="text" name="description" value="" id="description" />
 </form>
 
-<span onclick="addFormRow()">Add form row</span></br>
+<span class="button blue" id="addRow">Add form row</span></br>
 <span onClick='submitFormJSON("form", "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions/", "POST")'>Save</span>
