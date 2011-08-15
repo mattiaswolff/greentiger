c061c2e9e0b8d5ee013fbda896f7ba3cf1dcb74f
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-15 20:13:16 +0000

    user

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -22,7 +22,7 @@
             });
             
             $(".content").delegate("#save", "click", function(){
-                submitFormJSON("form", "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions/", "POST");
+                submitFormJSON("form", "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions", "POST");
             });
         });
 </script>
