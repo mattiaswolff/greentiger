4d84279b584c4b2858f08e42d39e28b799f8a7d0
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-15 19:24:07 +0000

    user

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -5,13 +5,4 @@
 </form>
 
 <span onclick="addFormRow()">Add form row</span></br>
-<?php 
-    if (!isset($_GET['definitionId'])) {
-        echo '<span onClick="' . "submitFormJSON('form', 'http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/users/" . $_GET['userId'] . "/definitions', 'POST')" . '">Save</span>';
-    }
-    else {
-        echo '<span onClick="' . "submitFormJSON('form,', 'http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions/" . $_GET['definitionId'] . "', 'PUT')" . '">Save</span>';
-    } 
-?>
-<br/>
-<span onClick=<?php echo (!isset($_GET['definitionId']) ? '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions', 'POST')" . '"' : '"' . "submitFormJSON('http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definition.php', 'PUT')" . '"' ); ?>>OldSave</span>';
+<span onClick='submitFormJSON("form", "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/definitions/", "POST")'>Save</span>
