0ee2bd829d1ee6ac33c5a4b54da901039f2c3d4e
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-16 18:46:41 +0000

    user

--- client/include/profile_content.php
+++ client/include/profile_content.php
@@ -1,5 +1,5 @@
 <form method="POST">
     <input type="text" name="name" value="" />
-    <textarea name="description"></description>
+    <textarea name="description"></textarea>
     <input class="button green" type="submit" name="PUT" value="Post" />
 </form>
