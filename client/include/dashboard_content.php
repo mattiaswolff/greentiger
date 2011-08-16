9728c7e6059dfb34ef9aa2bb4aa06afb41b9d156
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-16 18:02:37 +0000

    user

---
+++ client/include/dashboard_content.php
@@ -0,0 +1,10 @@
+<section class="createTask">
+    <div><ul class="horizontal"><li class="horizontal right"></li></ul></div>
+    <form class="task invisible" method="POST">
+        <div class="description left"></div>
+        <span class="button red right delete">X</span>
+        <section class="clear"></section>
+        <input class="button green" type="submit" name="POST" value="Post" />
+    </form>
+</section>
+<section class="taskFlow" />
