bcbe85a6294e54d4477296798adb589f491fb5cb
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-17 19:59:52 +0000

    user

--- git/auth.php
+++ git/auth.php
@@ -9,7 +9,6 @@ if (isset($_SESSION['userId'])) {
     $objUser->setAccessTokens($arrAccessTokens);
     $objUser->upsert();
     $strRedirectUri = (isset($_SESSION['redirectUri']) ? $_SESSION['redirectUri'] : "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/client/dashboard.php");  
-    die();
     header('Location: ' . $_SESSION['redirectUri'] . '#access_token=' . $arrNewAccessToken['token'] . "|" . $objUser->getId() . '&token_type=example&expires_in=4301');
 }
 else {
