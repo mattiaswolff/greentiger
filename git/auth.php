3df4e6dc36154bc835c1dca9e28debcaaa8365db
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-17 19:48:02 +0000

    user

--- git/auth.php
+++ git/auth.php
@@ -9,7 +9,7 @@ if (isset($_SESSION['userId'])) {
     $objUser->setAccessTokens($arrAccessTokens);
     $objUser->upsert();
     //header('Location: ' . $_SESSION['redirectUri'] . '#access_token=' . $arrNewAccessToken['token'] . "|" . $objUser->getId() . '&token_type=example&expires_in=4301');
-    header('Location: "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/client/dashboard.php");
+    header('Location: http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/client/dashboard.php');
 }
 else {
     header('Location: ' . $_SESSION['redirectUri'] . '#error=access_denied');
