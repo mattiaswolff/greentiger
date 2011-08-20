4b049167c0a5e8dde88412a632a5e4512cb5b8b7
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 19:16:22 +0000

    userus

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -1,4 +1,5 @@
 <form id="section">¨
     <div class="fields"></div>
     <div class="buttons"></div>
+    <div class="clear"></div>
 </form>
--- client/include/profile_content.php
+++ client/include/profile_content.php
@@ -1,4 +1,5 @@
 <form method="POST">
     <div class="fields"></div>
     <div class="buttons"></div>
+    <div class="clear"></div>
 </form>
