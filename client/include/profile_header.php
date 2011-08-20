04f7326b474aa47dc37836a6bd3f8712ad3a4842
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 16:30:56 +0000

    user

--- client/include/profile_header.php
+++ client/include/profile_header.php
@@ -6,7 +6,7 @@
             $('form').append(getHtmlTaskRow('description', 'description', 'textarea', '', true));
             $('input[name|="id"]').attr('value', window.sessionStorage.getItem("userId"));
             $('input[name|="name"]').attr('value', window.sessionStorage.getItem("userName"));
-            $('textarea[name|="description"]').append(window.sessionStorage.getItem("userName"));
+            $('textarea[name|="description"]').append(window.sessionStorage.getItem("userDescription"));
             $('form').append('<input class="button green" type="submit" name="PUT" value="Post" />');
         });
 	</script>
