ff77d8716660e6f46f7ce57bcc97551dfe825181
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 13:36:30 +0000

    user

--- client/master.php
+++ client/master.php
@@ -24,7 +24,7 @@
         }
         
         $(document).ready(function(){
-            if (null == window.localStorage.getItem("userId")) {
+            if (window.localStorage.getItem("userId") === null) {
                 $.ajax({  
                     url: getUrlApi("users/" + strUserId),  
                     dataType: 'json',  
