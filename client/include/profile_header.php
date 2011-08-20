d917a57d35583cc6f38c960312ad2e7e4e190d13
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 16:25:14 +0000

    user

--- client/include/profile_header.php
+++ client/include/profile_header.php
@@ -1,9 +1,12 @@
 <script type="text/javascript">
     //Create dashboard boxes (NOT IN USE) and Create titles.
         $(document).ready(function(){
-            $('form').append(getHtmlTaskRow('userId', 'description', 'text', '', true));
+            $('form').append(getHtmlTaskRow('id', 'description', 'text', '', true));
             $('form').append(getHtmlTaskRow('name', 'description', 'text', '', true));
             $('form').append(getHtmlTaskRow('description', 'description', 'textarea', '', true));
+            $('input[name|="id"]').attr('value', window.sessionStorage.getItem("userId");
+            $('input[name|="name"]').attr('value', window.sessionStorage.getItem("userName");
+            $('textarea[name|="description"]').append(window.sessionStorage.getItem("userName"));
             $('form').append('<input class="button green" type="submit" name="PUT" value="Post" />');
         });
 	</script>
