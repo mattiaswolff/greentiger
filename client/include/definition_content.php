99176021d68ddd2a3128e7635ac2638c94325e47
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 18:40:28 +0000

    userus

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -1,6 +1,9 @@
-<form id="section">Name: 
-    <input type="text" name="name" value="" id="name" />
-    Description: 
-    <input type="text" name="description" value="" id="description" />
+<form id="section">¨
+    <div class="fields">
+        Name: 
+        <input type="text" name="name" value="" id="name" />
+        Description: 
+        <input type="text" name="description" value="" id="description" />
+    </div>
     <div class="buttons"></div>
 </form>
