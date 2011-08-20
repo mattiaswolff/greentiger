4e26a1a0ef43ee41bdac1978d907505a017e7bb1
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 17:39:20 +0000

    userus

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -2,6 +2,6 @@
     <input type="text" name="name" value="" id="name" />
     Description: 
     <input type="text" name="description" value="" id="description" />
+    <span class="button blue" id="addRow">Add form row</span></br>
 </form>
-
-<span class="button blue" id="addRow">Add form row</span></br>
+//test
