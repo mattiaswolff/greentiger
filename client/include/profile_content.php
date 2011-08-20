70aced3522675c2ace104905e9fdfea5f250f85e
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 18:55:00 +0000

    userus

--- client/include/profile_content.php
+++ client/include/profile_content.php
@@ -1,2 +1,4 @@
 <form method="POST">
+    <div class="fields"></div>
+    <div class="buttons"></div>
 </form>
--- client/include/profile_header.php
+++ client/include/profile_header.php
@@ -1,13 +1,13 @@
 <script type="text/javascript">
     //Create dashboard boxes (NOT IN USE) and Create titles.
         $(document).ready(function(){
-            $('form').append(getHtmlTaskRow('id', 'description', 'text', '', true));
-            $('form').append(getHtmlTaskRow('name', 'description', 'text', '', true));
-            $('form').append(getHtmlTaskRow('description', 'description', 'textarea', '', true));
+            $('form .fields').append(getHtmlTaskRow('id', 'description', 'text', '', true));
+            $('form .fields').append(getHtmlTaskRow('name', 'description', 'text', '', true));
+            $('form .fields').append(getHtmlTaskRow('description', 'description', 'textarea', '', true));
             $('input[name|="id"]').attr('value', window.sessionStorage.getItem("userId"));
             $('input[name|="name"]').attr('value', window.sessionStorage.getItem("userName"));
             $('textarea[name|="description"]').append(window.sessionStorage.getItem("userDescription"));
-            $('form').append('<input class="button green" type="submit" name="PUT" value="Post" />');
+            $('form .buttons').append('<input class="button green" type="submit" name="PUT" value="Post" />');
             $('form').attr('url', getUrlApi("/users/"+ window.sessionStorage.getItem("userId")));
         });
         
