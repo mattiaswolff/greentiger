ca56bebab33bd546232d6aa2ce81acddd493de61
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 15:50:45 +0000

    user

--- client/include/profile_header.php
+++ client/include/profile_header.php
@@ -2,6 +2,7 @@
     //Create dashboard boxes (NOT IN USE) and Create titles.
         $(document).ready(function(){
             $('input[name="name"]').val(jsonUser.name);
-            $('textarea').append(jsonUser.description); 
+            $('textarea').append(jsonUser.description);
+            $('form').append(getHtmlTaskRow('name', description, 'text', '', true));
         });
 	</script>
