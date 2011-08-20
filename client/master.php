e17af9d9c8c5ae5f33c5af5352e169f938a030a7
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 16:49:57 +0000

    user

--- client/master.php
+++ client/master.php
@@ -45,6 +45,7 @@
                     data: {access_token: strAccessToken},
                     success: function(json){  
                         $('.userName').append(json.name);
+                        $('.userInfo .description').append(json.description);
                     }
             });
             
@@ -91,11 +92,6 @@
             <aside class="userInfo">
                 <img class="user" src="http://v1.fein.de/fein-multimaster/media/images/fein-multimaster/fein_company_logo.jpg" alt="Företaget" title="Företaget" border="0" />
                 <div class="description">This is a description of this user. We dont know that much about them yet though.</div>
-                <div class="links">
-                    <ul>
-                        <li><a href="http://www.dif.se">www.dif.se</a>
-                    </ul>
-                </div>
             </aside>
         <div class="main">
             <section class="userName"></section>
