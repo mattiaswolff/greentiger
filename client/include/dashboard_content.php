99c93db88ae470fc5be46a9a36833627255a47b1
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 19:00:02 +0000

    userus

--- client/include/dashboard_content.php
+++ client/include/dashboard_content.php
@@ -2,9 +2,8 @@
     <div><ul class="horizontal"><li class="horizontal right"></li></ul></div>
     <form class="task invisible" method="POST">
         <div class="description left"></div>
-        <span class="button red right delete">X</span>
         <section class="clear"></section>
-        <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" /><div>
+        <div class="buttons"><span class="button red right delete">X</span><input class="button green" type="submit" name="POST" value="Post" /><div>
     </form>
 </section>
 <section class="taskFlow" />
