047d9c75573e0006d0a0d84612bd071907bb92ca
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 19:13:19 +0000

    userus

--- client/include/dashboard_content.php
+++ client/include/dashboard_content.php
@@ -4,6 +4,7 @@
         <div class="description left"></div>
         <section class="clear"></section>
         <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" /><span class="button red right delete">Close</span><div>
+        <div class="clear"></div>
     </form>
 </section>
 <section class="taskFlow" />
