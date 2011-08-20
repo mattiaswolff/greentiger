bbfc566265dd0b9395873f9048a1610a22ff12b8
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-20 20:47:35 +0000

    userus

--- js/jquery.greentiger.js
+++ js/jquery.greentiger.js
@@ -18,7 +18,7 @@ function getTaskFlow (strUserId, strAccessToken) {
         var arrHtml = new Array();
         $.each(json.results[0], function(key, value) {    
             var d = new Date(value.updatedDate);
-            arrHtml.push('<article><div class="left"><span class="button blue">Type</span></div><div class="story"><div class="header">2011-04-13 Created by <span class="link">' + value.createdBy + '</span></div><div class="content">');
+            arrHtml.push('<article><div class="left"><span class="blue">Type</span></div><div class="story"><div class="header">2011-04-13 Created by <span class="link">' + value.createdBy + '</span></div><div class="content">');
             $.each(value.content, function (key1, value1) {
                 arrHtml.push('<span class="title">'+ key1 +':</span> '+ value1 +' / ');
             });
