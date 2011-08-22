f1ae2704c8155ff4036c859e76e9454093770060
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-22 20:16:28 +0000

    users

--- classes/user.php
+++ classes/user.php
@@ -20,7 +20,7 @@ class User {
             $this->_id = $arrResults['_id'];
             $this->name = $arrResults['name'];
             $this->email = $arrResults['email'];
-            $this->descriptions = $arrResults['descriptions'];
+            $this->description = $arrResults['descriptions'];
             $this->definitions = $arrResults['definitions'];
             $this->client_id = $arrResults['client_id'];
             $this->redirect_uri = $arrResults['redirect_uri'];
