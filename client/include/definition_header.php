a9cb0d43ed080ffb50f6faec21aebec30062fc28
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-22 20:00:51 +0000

    users

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -23,8 +23,8 @@
             $('form').attr('method', 'POST');
             
             $(".content").delegate("#addRow", "click", function(){
-                var counter = $('formRow').length;
-    	        var strHtml = '<article><div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <input type="text" name="content[' + counter + '].description" value="" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select> Config: <input type="text" name="content[' + counter + '].config" value="" /></div></article>';
+                var counter = $('.formRow').length;
+    	        var strHtml = '<article class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <input type="text" name="content[' + counter + '].description" value="" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select> Config: <input type="text" name="content[' + counter + '].config" value="" /></article>';
 		        $("form .fields").append(strHtml);
             });
             
