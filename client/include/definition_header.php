f3c55af12eda41414807771cdea94d850a59f58d
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-22 19:45:06 +0000

    users

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -20,15 +20,22 @@
             $('form .buttons').append('<input class="button green" type="submit" name="POST" value="Post" />');
             $('form .buttons').append('<span class="button blue" id="addRow">Add form row</span>');
             $('form').attr('url', getUrlApi("/users/"+ window.sessionStorage.getItem("userId") + "/definitions"));
-        
+            $('form').attr('method', 'POST');
+            
             $(".content").delegate("#addRow", "click", function(){
                 var counter = $('formRow').length;
     	        var strHtml = '<article><div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <input type="text" name="content[' + counter + '].description" value="" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select> Config: <input type="text" name="content[' + counter + '].config" value="" /></div></article>';
 		        $("form .fields").append(strHtml);
             });
             
-            $(".content").delegate("input[name=Post]", "click", function(){
-                submitFormJSON("form", "http://ec2-79-125-49-128.eu-west-1.compute.amazonaws.com/greentiger/api/users/" + strUserId + "/definitions", "POST", false);
-            });
+        $("body").delegate("form", "submit", function(event) {
+            if (event.preventDefault()) {
+                event.preventDefault();// cancels the form submission
+            }
+            else {
+                event.returnValue = false;
+            }
+            submitFormJSON(this, $(this).attr('url'), $(this).attr('method'), false);
+        });
         });
 </script>
