1f337cc15761e6f65db631845a8db044f5474f93
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 11:31:11 +0000

    mw

--- client/signup.php
+++ client/signup.php
@@ -1,4 +1,11 @@
 <?php
+   require_once(dirname(__FILE__) . '/handler/gitLoginHandler.php');
+        require_once(dirname(__FILE__) . '/util/gitConfig.php');
+        require_once(dirname(__FILE__) . '/util/gitApiClient.php');
+        require_once(dirname(__FILE__) . '/util/gitContext.php');
+        require_once(dirname(__FILE__) . '/AccountService.php');
+        require_once(dirname(__FILE__) . '/SessionManager.php');
+ 
     $sessionManager = gitContext::getSessionManager();
     $idpAssertion = $sessionManager->getAssertion();
     $page_content = file_get_contents("./include/signup_content.php");
