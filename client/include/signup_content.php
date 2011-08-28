f266e4141bc547104520f560ef911739e180e4a1
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 13:45:51 +0000

    mw

--- client/include/signup_content.php
+++ client/include/signup_content.php
@@ -3,6 +3,6 @@
         Name: <input type="text" name="name" maxlength="30" value=""/><br/>
         Email: <input type="text" name="email" value=""/><br/>
         UserId: <input id="userId" type="text" name="userId" value="" /><br/>
-        <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" onClick="submitFormJSON('form', getUrlApi('users'), 'POST', true)" /><div>
+        <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" /><div>
     </form>
 </section>
--- client/include/signup_header.php
+++ client/include/signup_header.php
@@ -12,5 +12,22 @@
 $(document).ready(function(){
         $('input[name="name"]').attr('value', '<?php echo $idpAssertion->getFirstName(); ?>');
         $('input[name="email"]').attr('value', '<?php echo $idpAssertion->getVerifiedEmail(); ?>');
+        $('form').attr('url', getUrlApi("users"));
+        /*
+        Purpose: Submit form by JSON
+        Created: 2011-08-28 (Mattias Wolff)
+        Updated: -
+        */
+        $("body").delegate("form", "submit", function(event) {
+            if (event.preventDefault()) {
+                event.preventDefault();// cancels the form submission
+            }
+            else {
+                event.returnValue = false;
+            }
+            
+            submitFormJSON(this ,$(this).attr('url'), $(this).attr('method'), false);
+        });
+        
     });        
 </script>
