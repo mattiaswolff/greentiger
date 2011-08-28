1f9944ff87d69a58283d17f9bf6a90f9b48b0fe2
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 12:39:05 +0000

    mw

--- client/signup.php
+++ client/signup.php
@@ -1,14 +1,5 @@
 <?php
-    require_once('../git/handler/gitLoginHandler.php');
-    require_once('../git/util/gitConfig.php');
-    require_once('../git/util/gitApiClient.php');
-    require_once('../git/util/gitContext.php');
-    require_once('../git/AccountService.php');
-    require_once('../git/SessionManager.php');
- 
-    $sessionManager = gitContext::getSessionManager();
-    $idpAssertion = $sessionManager->getAssertion();
-    $page_content = include("./include/signup_content.php");
-    $page_header = include("./include/signup_header.php");
+    $page_content = "./include/signup_content.php";
+    $page_header = "./include/signup_header.php";
     include('master.php');
 ?>
