6e7e0f5e9ce7a1db12ad2c28bf2e1211715a4163
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 12:51:57 +0000

    mw

--- client/include/signup_header.php
+++ client/include/signup_header.php
@@ -1,5 +1,9 @@
 <script type="text/javascript">
+<?php 
+        $sessionManager = gitContext::getSessionManager();
+        $idpAssertion = $sessionManager->getAssertion();
+    ?>
 $(document).ready(function(){
-        $('input[name="name"]').attr('value', 'test');
+        $('input[name="name"]').attr('value', '<?php echo $idpAssertion->getVerifiedEmail(); ?>');
     });        
 </script>
