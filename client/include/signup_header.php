8d1d1b5fe6ece2192bb3c84d68cd4f64e9789894
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 12:53:44 +0000

    mw

--- client/include/signup_header.php
+++ client/include/signup_header.php
@@ -1,8 +1,15 @@
 <script type="text/javascript">
-<?php 
-        $sessionManager = gitContext::getSessionManager();
-        $idpAssertion = $sessionManager->getAssertion();
-    ?>
+<?php
+    require_once('../git/handler/gitLoginHandler.php');
+    require_once('../git//util/gitConfig.php');
+    require_once('../git//util/gitApiClient.php');
+    require_once('../git//util/gitContext.php');
+    require_once('../git//AccountService.php');
+    require_once('../git//SessionManager.php');
+    
+    $sessionManager = gitContext::getSessionManager();
+    $idpAssertion = $sessionManager->getAssertion();
+?>
 $(document).ready(function(){
         $('input[name="name"]').attr('value', '<?php echo $idpAssertion->getVerifiedEmail(); ?>');
     });        
