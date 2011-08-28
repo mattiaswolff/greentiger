91cde89f4dba0a9ec9837cb8aff592b21742603c
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 12:33:25 +0000

    mw

--- client/signup.php
+++ client/signup.php
@@ -8,7 +8,7 @@
  
     $sessionManager = gitContext::getSessionManager();
     $idpAssertion = $sessionManager->getAssertion();
-    $page_content = file_get_contents("./include/signup_content.php");
-    $page_header = file_get_contents("./include/signup_header.php");
+    $page_content = include("./include/signup_content.php");
+    $page_header = include("./include/signup_header.php");
     include('master.php');
 ?>
