2c8e875c633618bcbcc25b704d062daad6b5be75
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 11:10:48 +0000

    mw

---
+++ client/include/signup_content.php
@@ -0,0 +1,8 @@
+<section>
+    <form class="task" method="POST">
+        Name: <input type="text" name="name" maxlength="30" value=""/><br/>
+        Email: <input type="text" name="email" value=""/><br/>
+        UserId: <input id="userId" type="text" name="userId" value="" /><br/>
+        <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" onClick="submitFormJSON('http://ec2-46-51-141-34.eu-west-1.compute.amazonaws.com/greentiger/api/users', 'POST')" /><div>
+    </form>
+</section>
