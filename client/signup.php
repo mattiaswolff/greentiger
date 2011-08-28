18ef8d2c73e699602e79c4914f4fb62296822040
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 12:49:22 +0000

    mw

--- client/signup.php
+++ client/signup.php
@@ -4,7 +4,8 @@
         include($path);
         return ob_get_clean();
     }
-    $page_content = ob_get_clean("./include/signup_content.php");
-    $page_header = ob_get_clean("./include/signup_header.php");
+    
+    $page_content = include_get_contents("./include/signup_content.php");
+    $page_header = include_get_contents("./include/signup_header.php");
     include('master.php');
 ?>
