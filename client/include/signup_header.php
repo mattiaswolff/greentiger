c02de0f5263d882b0dc0cf5ce9b925fd49a925d2
Mattias Wolff <mattias.wolff@gmail.com>
2011-08-28 11:33:49 +0000

    mw

--- client/include/signup_header.php
+++ client/include/signup_header.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
 $(document).ready(function(){
-        $('input[name="name"]').attr('value', "<?php echo $idpAssertion->getVerifiedEmail(); ?>");
+        $('input[name="name"]').attr('value', '<?php echo $idpAssertion->getVerifiedEmail(); ?>');
     });        
 </script>
