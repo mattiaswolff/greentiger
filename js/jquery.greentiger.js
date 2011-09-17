a4718cd57ac336c64adce5422bc5920142fe2440
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-17 17:30:36 +0000

    user

--- js/jquery.greentiger.js
+++ js/jquery.greentiger.js
@@ -24,7 +24,7 @@ function getTaskFlow (strUserId, strAccessToken) {
             });
             arrHtml.push('</div><div class="actions"> <span class="link" id="' + value._id + '">comment</span> (10) <span class="link" id="' + value._id + '">like</span> (3) </div><div class="comments">');
             $.each(value.comments, function (key1, value1) {
-                arrHtml.push('comment:' + value1);
+                arrHtml.push(value1.name + ' ' + value1.comment);
             });
             
             arrHtml.push('<form class="task" method="PUT" url="'+ getUrlApi("tasks/" + value._id + "?part=comments")+'"><input type="text" name="comments.userId" value="' + window.sessionStorage.getItem("userId") + '" /><input type="text" name="comments.comment" /><input class="invisible" type="submit"/></form></div></article>');
