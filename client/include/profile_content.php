f41ed57a393636b9920c2237596ecd031393a607
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-17 20:12:53 +0000

    user

--- client/include/profile_content.php
+++ client/include/profile_content.php
@@ -1,4 +1,9 @@
-<form method="POST">
+<form id="userInfo" method="PUT">
+    <div class="fields"></div>
+    <div class="buttons"></div>
+    <div class="clear"></div>
+</form>
+<form id="image" method="PUT">
     <div class="fields"></div>
     <div class="buttons"></div>
     <div class="clear"></div>
--- client/include/profile_header.php
+++ client/include/profile_header.php
@@ -1,18 +1,18 @@
 <script type="text/javascript">
     //Create dashboard boxes (NOT IN USE) and Create titles.
         $(document).ready(function(){
-            $('form .fields').append(getHtmlTaskRow('id', 'id', 'description', 'text', '', true));
-            $('form .fields').append(getHtmlTaskRow('name', 'name', 'description', 'text', '', true));
-            $('form .fields').append(getHtmlTaskRow('description', 'description', 'description', 'textarea', '', true));
+            $('#userInfo .fields').append(getHtmlTaskRow('id', 'id', 'description', 'text', '', true));
+            $('#userInfo .fields').append(getHtmlTaskRow('name', 'name', 'description', 'text', '', true));
+            $('#userInfo .fields').append(getHtmlTaskRow('description', 'description', 'description', 'textarea', '', true));
             $('input[name|="id"]').attr('value', window.sessionStorage.getItem("userId"));
             $('input[name|="name"]').attr('value', window.sessionStorage.getItem("userName"));
                         $('textarea[name|="description"]').append(window.sessionStorage.getItem("userDescription"));
                         $('input[name|="id"]').attr("disabled", true);
-            $('form .buttons').append('<input class="button green" type="submit" name="PUT" value="Post" />');
+            $('#userInfo .buttons').append('<input class="button green" type="submit" name="PUT" value="Post" />');
             $('form').attr('url', getUrlApi("users/"+ window.sessionStorage.getItem("userId")));
             $('form').attr('method', 'PUT');
         
-            $("body").delegate("form", "submit", function(event) {
+            $("body").delegate("#userInfo", "submit", function(event) {
                 if (event.preventDefault()) {
                     event.preventDefault();// cancels the form submission
                 }
