d36c1f819a27bdbf44933d3ff1ca306ddc6ac449
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-18 14:45:58 +0000

    user

--- git/userstatus.php
+++ git/userstatus.php
@@ -3,6 +3,6 @@ require "../classes/rest.php";
 require "../classes/user.php";
 
 $data = RestUtils::processRequest();
-$array = array("registered" => true);
+$array = array("registered" => false);
 RestUtils::sendResponse(200, $array, 'application/json');
 ?>
