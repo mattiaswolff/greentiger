76cf88335d49bb701e05ffb4471810c9edc68d28
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-18 15:00:53 +0000

    user

--- git/userstatus.php
+++ git/userstatus.php
@@ -3,7 +3,17 @@ require "../classes/rest.php";
 require "../classes/user.php";
 
 $data = RestUtils::processRequest();
-
+$arrRequestVars = $data->getRequestVars();
+$strEmail = $arrRequestVars["email"];
+$m = new Mongo();
+$db = $m->projectcopperfield;
+$arrResults = $db->users->findOne(array("email" => $strEmail);
+if ($arrResults != null) {
+    $array = array("registered" => true);
+}
+else {
+    $array = array("registered" => false);
+}
 $array = array("registered" => false);
 RestUtils::sendResponse(200, $array, 'application/json');
 ?>
