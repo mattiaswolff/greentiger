ea2adbdb7bdd1a8b0301b6860a7e71c80dcb33b2
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-18 15:03:31 +0000

    user

--- git/userstatus.php
+++ git/userstatus.php
@@ -14,6 +14,5 @@ if ($arrResults != null) {
 else {
     $array = array("registered" => false);
 }
-$array = array("registered" => false);
 RestUtils::sendResponse(200, $array, 'application/json');
 ?>
