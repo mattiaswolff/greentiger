9564cd09efd29672562ea6612730bd3ab0e24632
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-18 15:23:45 +0000

    user

--- client/include/signup_content.php
+++ client/include/signup_content.php
@@ -2,9 +2,9 @@
     <form>
         Email: <input type="email" name="email" value=""/>
         UserId: <input id="userId" type="text" name="userId" value="" />
-        Name: <input type="text" name="name" maxlength="30" value=""/><br/>
-        Password: <input type="text" name="password" maxlength="30" value=""/>
-        Repeate password: <input type="text" name="password2" maxlength="30" value=""/>
+        Name: <input type="text" name="name" maxlength="30" value=""/>
+        Password: <input type="password" name="password" maxlength="30" value=""/>
+        Repeate password: <input type="password" name="password2" maxlength="30" value=""/>
         <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" /><div>
     </form>
 </section>
