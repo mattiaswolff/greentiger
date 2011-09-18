b8274b93ab32fb5133aaa55aed89fa14ba7de684
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-18 11:05:09 +0000

    user

--- client/include/dashboard_header.php
+++ client/include/dashboard_header.php
@@ -116,7 +116,7 @@
         Updated: -
         */
         $(window).scroll(function(){
-        if  ($(window).scrollTop() == $(document).height() - $(window).height() - Math.round($(window).height()/4)){
+        if  ($(window).scrollTop() >= $(document).height() - $(window).height() - Math.round($(window).height()/4)){
            getTaskFlow (strUserId, strAccessToken, false);
         }
 });
