8f0bde6b325bd55cccc59e5e4ca9844ba99cc638
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-24 19:19:22 +0000

    login

--- git/auth2.php
+++ git/auth2.php
@@ -3,8 +3,17 @@ require "../classes/user.php";
 session_start();
 echo "test: " . print_r($_SESSION);
 
-if (isset($_SESSION['userId'])) {
-    $objUser = new User($_SESSION['userId']);
-    echo "<script type='text/javascript'>var userData = { email: 'name@idp.com', displayName: 'User Name', photoUrl: 'http://website.com/img/user.png'}; window.google.identitytoolkit.updateSavedAccount(userData); </script>";
+if (isset($_SESSION['userId'])) {?>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/googleapis/0.0.4/googleapis.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/jsapi"></script>
+<script type="text/javascript">
+  google.load("identitytoolkit", "1.0", {packages: ["ac"]});
+</script>
+    <script type='text/javascript'>
+        var userData = { email: 'name@idp.com', displayName: 'User Name', photoUrl: 'http://website.com/img/user.png'}; window.google.identitytoolkit.updateSavedAccount(userData); 
+    </script>
+    <?php
 }
 ?>
