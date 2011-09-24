c14ab82a380b9e5ceabd2094e0f769d76c40751e
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-24 16:45:37 +0000

    login

--- git/auth2.php
+++ git/auth2.php
@@ -4,7 +4,7 @@ require "../classes/user.php";
 
 if (isset($_SESSION['userId'])) {
     $objUser = new User($_SESSION['userId']);
-    echo "<script type="text/javascript">var userData = {email: 'name@idp.com',displayName: 'User Name',photoUrl: 'http://website.com/img/user.png'};window.google.identitytoolkit.updateSavedAccount(userData);</script>";
+    echo "<script type='text/javascript'>var userData = {email: 'name@idp.com',displayName: 'User Name',photoUrl: 'http://website.com/img/user.png'};window.google.identitytoolkit.updateSavedAccount(userData);</script>";
     $arrAccessTokens = $objUser->getAccessTokens(); 
     $arrNewAccessToken = array("token" => md5(mt_rand()), "createdDate" => new MongoDate());
     $arrAccessTokens[] = $arrNewAccessToken;
