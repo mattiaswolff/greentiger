81d15b0e1e59b8de35db0211f0f7ed9c2ef047b3
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-24 16:49:31 +0000

    login

--- git/auth2.php
+++ git/auth2.php
@@ -5,6 +5,5 @@ require "../classes/user.php";
 if (isset($_SESSION['userId'])) {
     $objUser = new User($_SESSION['userId']);
     echo "<script type='text/javascript'>var userData = {email: 'name@idp.com',displayName: 'User Name',photoUrl: 'http://website.com/img/user.png'};window.google.identitytoolkit.updateSavedAccount(userData);</script>";
-    echo "test";
 }
 ?>
