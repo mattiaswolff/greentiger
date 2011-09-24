5e5d46fce81081499d388a27526f32023ce081e4
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-24 19:13:02 +0000

    login

--- git/auth2.php
+++ git/auth2.php
@@ -5,6 +5,6 @@ echo "test: " . print_r($_SESSION);
 
 if (isset($_SESSION['userId'])) {
     $objUser = new User($_SESSION['userId']);
-    echo "<script type='text/javascript' src='https://ajax.googleapis.com/jsapi'></script><script type='text/javascript'> google.load('identitytoolkit', '1.0');</script><script type='text/javascript'>var userData = {email: 'name@idp.com',displayName: 'User Name',photoUrl: 'http://website.com/img/user.png'};window.google.identitytoolkit.updateSavedAccount(userData);</script>";
+    echo "<script type='text/javascript'>var userData = { email: 'name@idp.com', displayName: 'User Name', photoUrl: 'http://website.com/img/user.png'}; window.google.identitytoolkit.updateSavedAccount(userData); </script>";
 }
 ?>
