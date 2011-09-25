bb9cca392604b50390274009ae6a0e2847ef4398
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-25 15:21:38 +0000

    upload

--- git/auth2.php
+++ git/auth2.php
@@ -29,8 +29,7 @@ if (isset($_SESSION['userId'])) {
         window.google.identitytoolkit.updateSavedAccount(userData); 
     </script>
     <?php
+    die();
     header('Location: ' . $strRedirectUri);
 }
-
-
 ?>
