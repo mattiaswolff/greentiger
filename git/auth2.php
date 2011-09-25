39705c9b2ea4008db152847910127828ef00c642
Mattias Wolff <mattias.wolff@gmail.com>
2011-09-25 15:23:24 +0000

    upload

--- git/auth2.php
+++ git/auth2.php
@@ -20,7 +20,7 @@ if (isset($_SESSION['userId'])) {
     </script>
     <script type="text/javascript" src="../js/jquery.greentiger.js"></script>
     <script type='text/javascript'>
-        var strUrlImg = getUrlApi("users/" + "matwo01" + "?part=image");
+        var strUrlImg = getUrlApi("users/" + "<?php echo $objUser->getId(); ?>" + "?part=image");
         var userData = { 
             email: "<?php echo $objUser->getEmail(); ?>", 
             displayName: "<?php echo $objUser->getName();?>", 
