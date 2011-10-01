8ea7870fc5c5df573e492159e29d661c50078c66
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-01 11:12:43 +0000

    login

--- client/logout.php
+++ client/logout.php
@@ -2,4 +2,4 @@
      var strUrlRedirect = "http://ec2-46-51-141-34.eu-west-1.compute.amazonaws.com/greentiger/client/dashboard.php?userId=" + sessionStorage.getItem('userId');
      sessionStorage.clear();
      window.location = strUrlRedirect; 
-</script>//test
+</script>
