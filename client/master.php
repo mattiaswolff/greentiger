191e649b6dc3085f4bfaca483c0b9e6b5be36e7d
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-01 18:11:10 +0000

    login

--- client/master.php
+++ client/master.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="../css/main.css"></link>
 </head>
 
