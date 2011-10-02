ee60d057e283bbf1a465045fbed19708047769ed
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-02 12:36:42 +0000

    dashboard

--- js/embed.js
+++ js/embed.js
@@ -32,26 +32,23 @@ var projectCopperfield = {
         get_or_create_div : function(c) {
             var b = document.getElementById(c);
             if (!b) {
-                var a=document.getElementsByTagName("body")[0];
-                var b=document.createElement("div"); 
-                b.id=c; 
+                var a = document.getElementsByTagName("body")[0];
+                var b = document.createElement("div"); 
+                b.id = c; 
                 a.appendChild(b)
             } 
             return b
         },
         toggle_box : function() {
             var a = document.getElementById("projectCopperfield_container"); 
-            if (a) { 
-                projectCopperfield.delete_frame() 
-            } 
-            else { 
-                a = projectCopperfield.get_or_create_div("projectCopperfield_container"); 
+            if (a) {projectCopperfield.delete_frame()} 
+            else { a = projectCopperfield.get_or_create_div("projectCopperfield_container"); 
                 projectCopperfield.create_overlay(a); 
                 projectCopperfield.create_frame(a)
             }
         }, 
         create_overlay: function(b) { 
-            var a="top: 0; bottom:0; min-height:600px; left: 0; background: #000;filter: alpha(opacity=70); opacity: 0.7; z-index: 1000; width: 100%;";
+            var a="top: 0; bottom:0; min-height:600px; left: 0; background: #000;filter: alpha(opacity=65); opacity: 0.65; z-index: 1000; width: 100%;";
             a += projectCopperfield.get_fixed_style();
             if (projectCopperfield.old_browser()) { 
                 var c = Math.max(document.body.clientHeight,document.documentElement.clientHeight); 
@@ -66,7 +63,17 @@ var projectCopperfield = {
             var a = projectCopperfield.custom_image_url || projectCopperfield.static_base_url + projectCopperfield.image_url + "-" + projectCopperfield.lang + "-" + projectCopperfield.image_align + ".png"; 
             b.innerHTML += '<img src="' + a + '" alt="" id="projectCopperfield_feedback" style="'+c+'" onclick="projectCopperfield.toggle_box()">'
         },
-        create_frame:function(d){var b=projectCopperfield.close_text[projectCopperfield.lang];var e="cursor:pointer; height:26px; left:50%; margin-left:376px; top:9px; width:90px; z-index:100100;";e+=projectCopperfield.get_fixed_style();d.innerHTML+='<img title="'+b+'" src="'+projectCopperfield.base_url+projectCopperfield.btn_close_url+projectCopperfield.lang+'.gif" alt="" id="projectCopperfield_close" style="'+e+'" onclick="projectCopperfield.delete_frame()">';var c=projectCopperfield.base_url+projectCopperfield.embed_url+projectCopperfield.org_slug+"?lang="+projectCopperfield.lang;if(projectCopperfield.user_name&&projectCopperfield.user_email){c+="&name="+projectCopperfield.user_name+"&useremail="+projectCopperfield.user_email;c=encodeURI(c)}var a="background: white; border: none; height: 540px; left: 50%; margin-left: -470px; position: fixed; top: 40px; width: 940px; z-index: 100000;";a+=projectCopperfield.get_fixed_style();d.innerHTML+='<iframe src="'+c+'" id="projectCopperfield_iframe" scrolling="no" frameborder="no" style="'+a+'"></iframe>'},delete_frame:function(){var a=projectCopperfield.get_or_create_div("projectCopperfield_container");a.parentNode.removeChild(a)},settings_value_exists:function(a){return _projectCopperfield[a]&&(typeof _projectCopperfield[a]==="string")},is_valid_lang:function(){var a=_projectCopperfield.lang;return a==="en"||a==="sv"||a==="da"||a==="no"||a==="de"},is_valid_align:function(){var a=_projectCopperfield.align;return a==="right"||a==="left"},init:function(){projectCopperfield.org_slug=_projectCopperfield.org;if(projectCopperfield.settings_value_exists("align")&&projectCopperfield.is_valid_align()){projectCopperfield.image_align=_projectCopperfield.align}if(projectCopperfield.settings_value_exists("top")){projectCopperfield.image_top=_projectCopperfield.top}if(projectCopperfield.settings_value_exists("lang")&&projectCopperfield.is_valid_lang()){projectCopperfield.lang=_projectCopperfield.lang}if(projectCopperfield.settings_value_exists("image"))
+        create_frame : function(d) {
+            var b = projectCopperfield.close_text[projectCopperfield.lang];
+            var e = "cursor:pointer; height:26px; left:50%; margin-left:376px; top:9px; width:90px; z-index:100100;"; 
+            e += projectCopperfield.get_fixed_style(); 
+            d.innerHTML += '<img title="' + b + '"src="' + projectCopperfield.base_url + projectCopperfield.btn_close_url + projectCopperfield.lang + '.gif" alt="" id="projectCopperfield_close" style="' + e + '" onclick="projectCopperfield.delete_frame()">'; 
+            var c = projectCopperfield.base_url + projectCopperfield.embed_url + projectCopperfield.org_slug + "?lang=" + projectCopperfield.lang; 
+        if (projectCopperfield.user_name && projectCopperfield.user_email) { 
+            c+= "&name=" + projectCopperfield.user_name + "&useremail=" + projectCopperfield.user_email; 
+            c = encodeURI(c)
+            } 
+            var a="background: white; border: none; height: 540px; left: 50%; margin-left: -470px; position: fixed; top: 40px; width: 940px; z-index: 100000;";a+=projectCopperfield.get_fixed_style();d.innerHTML+='<iframe src="'+c+'" id="projectCopperfield_iframe" scrolling="no" frameborder="no" style="'+a+'"></iframe>'},delete_frame:function(){var a=projectCopperfield.get_or_create_div("projectCopperfield_container");a.parentNode.removeChild(a)},settings_value_exists:function(a){return _projectCopperfield[a]&&(typeof _projectCopperfield[a]==="string")},is_valid_lang:function(){var a=_projectCopperfield.lang;return a==="en"||a==="sv"||a==="da"||a==="no"||a==="de"},is_valid_align:function(){var a=_projectCopperfield.align;return a==="right"||a==="left"},init:function(){projectCopperfield.org_slug=_projectCopperfield.org;if(projectCopperfield.settings_value_exists("align")&&projectCopperfield.is_valid_align()){projectCopperfield.image_align=_projectCopperfield.align}if(projectCopperfield.settings_value_exists("top")){projectCopperfield.image_top=_projectCopperfield.top}if(projectCopperfield.settings_value_exists("lang")&&projectCopperfield.is_valid_lang()){projectCopperfield.lang=_projectCopperfield.lang}if(projectCopperfield.settings_value_exists("image"))
             {projectCopperfield.custom_image_url=_projectCopperfield.image}
             if("https:"==document.location.protocol) {projectCopperfield.static_base_url="https://static-ssl.projectCopperfield.se";
             projectCopperfield.base_url="https://projectCopperfield.se"}
