3bb21d93b9dce5118e65aacf16c57864c71ec1e8
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-07 20:22:50 +0000

    external

--- client/embed.php
+++ client/embed.php
@@ -2,8 +2,31 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $.getJSON(getUrlApi('users/' + strUserId), {access_token: strAccessToken},function(json) {
+            $.each(json.definitions, function(key, value) {
+                //var counter = $('.definitions > article').length;
+                //var strHtml = '<article class="definition" id="dashboard_' + value._id.$id +'"><span class="header">' + value.name + '(<span class="total"></span>)</span><table><thead><tr><th>Updated</th><th>Title</th><th>C</th><th>L</th></tr></thead><tbody></tbody></table><a href="">View all</a></article>';
+                var strHtml2 = '<li class="horizontal"><span class="button blue" id="' + value._id.$id + '">' + value.name + '</span></li>';
+                //$('.definitions').append(strHtml);
+                $('section.createTask > div > ul').append(strHtml2); 
+            });
+            //$('article.definition:nth-child(odd)').addClass('left');
+            //$('article.definition:nth-child(even)').addClass('right');
+        });
+    </script>    
 </head>
 <body id="home">
-    This is a test!
+<section class="createTask">
+    <div><ul class="horizontal"><li class="horizontal right"></li></ul></div>
+    <form class="task invisible" method="POST">
+        <div class="description left"></div>
+        <section class="clear"></section>
+        <div class="buttons"><input class="button green" type="submit" name="POST" value="Post" /><span class="button red right delete">Close</span></div>
+        <div class="clear"></div>
+    </form>
+</section>
 </body>
 </html>
