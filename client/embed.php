b6602cf60d2703bed662edbcead849a5027a6c34
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-07 21:19:57 +0000

    external

--- client/embed.php
+++ client/embed.php
@@ -4,14 +4,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <script type="text/javascript" src="../js/jquery.greentiger.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../css/embed.css" />
     <script type="text/javascript">
     $(document).ready(function(){
         strUserId = getParameterByName("userId");
         $.getJSON(getUrlApi('users/' + strUserId),function(json) {
             $.each(json.definitions, function(key, value) {
-                //var counter = $('.definitions > article').length;
-                //var strHtml = '<article class="definition" id="dashboard_' + value._id.$id +'"><span class="header">' + value.name + '(<span class="total"></span>)</span><table><thead><tr><th>Updated</th><th>Title</th><th>C</th><th>L</th></tr></thead><tbody></tbody></table><a href="">View all</a></article>';
                 var strHtml2 = '<li class="horizontal"><span class="button blue" id="' + value._id.$id + '">' + value.name + '</span></li>';
                 //$('.definitions').append(strHtml);
                 $('section.createTask > div > ul').append(strHtml2); 
