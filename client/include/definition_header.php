89593a36d92cce7fe26b114eb42756cf34048dcf
Mattias Wolff <mattias.wolff@gmail.com>
2011-10-31 21:30:07 +0000

    user

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -11,7 +11,7 @@
                         var newrow = document.createElement('article');
                         var counter = document.getElementsByClassName('formRow').length;
                         newrow.innerHTML = '<div class="formRow">Name: <input type="text" name="content[' + counter + '].name" value="' + value.name + '" /> Description: <input type="text" name="content[' + counter + '].description" value="' + value.type + '" /> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select>Name: <input type="text" name="content[' + counter + '].config" value="' + value.config + '" /></div>';
-		                $("section .fields").append(newrow);
+		                $("#section .fields").append(newrow);
                     });
                 });
             }
