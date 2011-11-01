870c701b2b0cce78f77983f79aaa2bc528a1c15d
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-01 20:08:34 +0000

    user

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -13,7 +13,7 @@
                     
                     $.each(json.results[0].content, function(key, value) {
                         var newrow = document.createElement('article');
-                        var counter = document.getElementsByClassName('formRow').length;
+                        var counter = document.getElementsByClassName('edef').length;
                         newrow.innerHTML = '<div class="edef">Name: <input type="text" name="content[' + counter + '].name" value="' + value.name + '" /> Description: <textarea name="content[' + counter + '].description">' + value.type + '</textarea> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select>Config: <input type="text" name="content[' + counter + '].config" value="' + value.config + '" /></div>';
 		                $("#section .fields").append(newrow);
                     });
