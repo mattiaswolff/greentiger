3d9431626be51276562cb59380873187f8699e4d
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-01 20:38:39 +0000

    user

--- client/include/definition_header.php
+++ client/include/definition_header.php
@@ -25,8 +25,12 @@
             $('form .buttons').append('<input class="button green" type="submit" name="POST" value="Post" />');
             $('form .buttons').append('<span class="button blue" id="addRow">Add form row</span>');
             $('form').attr('url', getUrlApi("users/"+ window.sessionStorage.getItem("userId") + "/definitions"));
-            $('form').attr('method', 'POST');
-            
+            if (strDefinitionId = "") {
+                $('.edef form').attr('method', 'POST');
+            }
+            else {
+                $('.edef form').attr('method', 'PUT');
+            }
             $(".content").delegate("#addRow", "click", function(){
                 var counter = $('.edef-row').length;
     	        var strHtml = '<article class="edef-row">Name: <input type="text" name="content[' + counter + '].name" value="" /> Description: <textarea name="content[' + counter + '].description"></textarea> Type: <select name="content[' + counter + '].type"><option value="text">Text</option><option value="textarea">Textarea</option><option value="email">Email</option><option value="checkbox">Checkbox</option><option value="radio">Radio button</option><option value="date">Date</option><option value="range">Range</option><option value="url">URL</option><option value="number">Number</option><option value="time">Time</option><option value="dropdown">Drop Down</option></select>Config: <input type="text" name="content[' + counter + '].config" value="" /></article>';
