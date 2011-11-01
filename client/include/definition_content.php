f62a83021c1a78fe4cead8ef48e9875e6d2e382c
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-01 20:11:22 +0000

    user

--- client/include/definition_content.php
+++ client/include/definition_content.php
@@ -1,7 +1,7 @@
 <section class="odef">
 </section>
 New definition
-<form id="section">
+<form class="edef">
     <div class="fields"></div>
     <div class="buttons"></div>
     <div class="clear"></div>
