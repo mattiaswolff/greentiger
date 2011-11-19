0ccad21d90cb1f2282e91fb5c00d1739470f5db1
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-19 21:14:18 +0000

    user

--- client/master.php
+++ client/master.php
@@ -18,14 +18,7 @@
 </head>
 
 <body id="home">
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/sv_SE/all.js#xfbml=1&appId=214551768609754";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
         <div class ="top">
             <nav class="pageNav invisible">
                 <a id="a_home">home</a> | 
