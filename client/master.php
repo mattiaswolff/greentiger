11673d73c302c0862dbf05b04f9c197f70a4fdde
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-19 19:05:09 +0000

    user

--- client/master.php
+++ client/master.php
@@ -42,13 +42,12 @@
             </aside>
         <div class="main">
             <section class="userName"></section>
+            <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="arial"></div>
+            <g:plusone count="false"></g:plusone>
             <div class="content">
                 <?php include $page_content; ?>
             </div>
             <aside>
-                <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="arial"></div>
-                <g:plusone count="false"></g:plusone>
-                
             </aside>
         </div>
     </div>
