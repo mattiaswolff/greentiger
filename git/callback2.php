50625cf5cebe9811228823e7b962fccfa2d64597
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-26 20:57:47 +0000

    user

--- git/callback2.php
+++ git/callback2.php
@@ -57,9 +57,9 @@ class EasyRpService {
         }
         session_start();
         $_SESSION["userId"] = $strUserId;
-        echo $_SESSION["userId"];
+        echo "test: " $_SESSION["userId"];
         die();
-        //print
+        
         echo  "<script type='text/javascript' src='https://ajax.googleapis.com/jsapi'></script><script type='text/javascript'>google.load('identitytoolkit', '1.0', {packages: ['notify']});</script><script type='text/javascript'>window.google.identitytoolkit.notifyFederatedSuccess({'email': '" . $result["verifiedEmail"] ."', 'registered': true });</script>";
         return "OK";
     }
