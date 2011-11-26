7d8ec18fdd0d148321417203473ba12ddc99caf8
Mattias Wolff <mattias.wolff@gmail.com>
2011-11-26 22:15:30 +0000

    user

--- client/logout.php
+++ client/logout.php
@@ -1,5 +1,5 @@
 <script type='text/javascript'>
-     var strUrlRedirect = "http://ec2-46-51-141-34.eu-west-1.compute.amazonaws.com/greentiger/client/dashboard.php?userId=" + sessionStorage.getItem('userId');
+     var strUrlRedirect = "http://www.zowgle.com/client/dashboard.php?userId=" + sessionStorage.getItem('userId');
      sessionStorage.clear();
      window.location = strUrlRedirect; 
 </script>
