b467a66720204415156540986a60b561cca8671c
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-10 17:44:42 +0000

    user

--- client/email.php
+++ client/email.php
@@ -4,31 +4,11 @@ require "../AWSSDKforPHP/sdk.class.php";
 $email = new AmazonSES();
  
 $response = $email->send_email(
-    'no-reply@amazon.com', // Source (aka From)
-    array( // Destination (aka To)
-        'ToAddresses' => array(
-            'mattias.wolff@gmail.com',
-        ),
-        'CcAddresses' => 'mattias.wolff@medius.se'
-    ),
-    array( // Message (long form)
-        'Subject' => array(
-            'Data' => 'émåîl tést ' . time(),
-            'Charset' => 'UTF-8'
-        ),
-        'Body' => array(
-            'Text' => array(
-                'Data' => 'Thîs îs å plåîn téxt, ünîcødé tést méssåge ' . time(),
-                'Charset' => 'UTF-8'
-            ),
-            'Html' => array(
-                'Data' => '<p><strong>Thîs îs ån HTML, ünîcødé tést méssåge ' . time() . '</strong></p>',
-                'Charset' => 'UTF-8'
-            )
-        )
-    ),
-    array( // Optional parameters
-        'ReplyToAddresses' => array('no-reply@amazon.com', 'nobody@amazon.com')
+    'no-reply@zowgle.com', // Source (aka From)
+    array('ToAddresses' => 'mattias.wolff@gmail.com'), // Destination (aka To)
+    array( // Message (short form)
+        'Subject.Data' => 'Email Test ' . time(),
+        'Body.Text.Data' => 'This is a simple test message ' . time()
     )
 );
  
