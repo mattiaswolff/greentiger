fcfb56fd6c1e10a5f32ec26be5b5999b74060423
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-10 17:58:44 +0000

    email

--- client/email.php
+++ client/email.php
@@ -4,7 +4,7 @@ require "../AWSSDKforPHP/sdk.class.php";
 $email = new AmazonSES();
 $email->setRegion(AmazonSES::REGION_US_E1);
 $response = $email->send_email(
-    'no-reply@zowgle.com', // Source (aka From)
+    'no-replay@zowgle.com', // Source (aka From)
     array('ToAddresses' => 'mattias.wolff@gmail.com'), // Destination (aka To)
     array( // Message (short form)
         'Subject.Data' => 'Email Test ' . time(),
