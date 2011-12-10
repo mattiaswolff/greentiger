d62ea521ea30d0ac66c06b635a4cfa7077792b08
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-10 11:54:58 +0000

    user

--- client/embed.php
+++ client/embed.php
@@ -73,7 +73,7 @@
         <div class="crt-desc"></div>
         <section class="clear"></section>
         <div class="crt-buttons">
-        <input type="email" name="strCreateUserEmail" />
+        <input type="email" name="createUserEmail" />
         <input class="button green" type="submit" name="POST" value="Post" /></div>
         <div class="clear"></div>
     </form>
