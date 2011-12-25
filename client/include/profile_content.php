9335ad65aef06c7b5ca0894d3eae14372ec38933
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-25 13:22:41 +0000

    test

--- client/include/profile_content.php
+++ client/include/profile_content.php
@@ -3,9 +3,3 @@
     <div class="buttons"></div>
     <div class="clear"></div>
 </form>
-
-<div id="container">
-    <div id="filelist"></div>
-    <a id="pickfiles" href="#">Select image</a>
-    <a id="uploadfiles" href="#">Upload image</a>
-</div>
