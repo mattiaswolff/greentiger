5dcd660774d77e66d4f2e9452ab952e09fcb2168
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-27 19:52:47 +0000

    user

--- classes/task.php
+++ classes/task.php
@@ -91,7 +91,7 @@ class Task {
         elseif ($arrObjectId != null) {
             print "test" . var_dump($arrSearch);
             if ($strSearch != '') {
-                $objResults[0] = $db->tasks->find("$and" => array("_id" => array('$in' => $arrObjectId), "keywords" => array('$in' => $arrSearch)))->sort(array("_id" => -1))->limit($intLimit)->skip($intSkip);
+                $objResults[0] = $db->tasks->find(array("$and" => array("_id" => array('$in' => $arrObjectId), "keywords" => array('$in' => $arrSearch))))->sort(array("_id" => -1))->limit($intLimit)->skip($intSkip);
                 
             }
             else {
