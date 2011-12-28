fc0606862811e6b61acff20ef274a460e560c03c
Mattias Wolff <mattias.wolff@gmail.com>
2011-12-28 21:09:15 +0000

    user

--- client/master.php
+++ client/master.php
@@ -58,6 +58,22 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/googleapis/0.0.4/googleapis.min.js"></script>
     <script type="text/javascript" src="//ajax.googleapis.com/jsapi"></script>
     <script type="text/javascript" src="http://updateyourbrowser.net/uyb.js"> </script>
+    <script type="text/javascript"> 
+   var _projectCopperfield = _projectCopperfield || {}; 
+   _projectCopperfield["id"] = "4eed1da92126029357000002"; 
+   _projectCopperfield["userName"] = ""; 
+   _projectCopperfield["userEmail"] = ""; 
+   _projectCopperfield["image"] = "https://lh5.googleusercontent.com/-HKLSAuPUlsY/TuK4EVE9crI/AAAAAAAAAAg/oz_WDgQQmw4/s101/contact_us.png"; 
+   (function() {function async_load(){ 
+      var s = document.createElement("script"); 
+      s.type = "text/javascript"; 
+      s.async = true; 
+      s.src = "http://www.zowgle.com/js/embed.js"; 
+      var x = document.getElementsByTagName("script")[0]; 
+      x.parentNode.insertBefore(s, x);} 
+      if (window.attachEvent)window.attachEvent("onload", async_load); 
+      else window.addEventListener("load", async_load, false); })(); 
+</script>
     <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-26662915-1']);
