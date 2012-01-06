550eec236a3283391bb1c93b8de1239e613c6b41
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-06 23:46:29 +0000

    user

--- git/auth2.php
+++ git/auth2.php
@@ -30,9 +30,7 @@ if (isset($_SESSION['userId'])) {
             photoUrl: strUrlImg,
         };
         window.google.identitytoolkit.updateSavedAccount(userData);
-        window.localStorage.setItem("email", "<?php echo $strEmail ?>");
-        window.localStorage.setItem("email", "<?php echo $strName ?>");
-        window.localStorage.setItem("email", strUrlImg);
+        window.localStorage.setItem("chooserAccounts", '[{"email": "<?php echo $strEmail ?>"}, {"displayName": "Mattias"}]');
     </script>
     <?php
     header('Location: ' . $strRedirectUri);
