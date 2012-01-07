bda470b4e1ee3c05d0143d9048ca7cef9bf74ee0
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-07 00:31:34 +0000

    user

--- git/auth2.php
+++ git/auth2.php
@@ -14,19 +14,19 @@ if (isset($_SESSION['userId'])) {
     $objUser->upsert();
     $strRedirectUri = "http://www.zowgle.com/client/dashboard.php?userId=" . $strId . "#access_token=" . $arrNewAccessToken['token'] . "|" . $strId . "&token_type=example&expires_in=4301";  
 ?>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/googleapis/0.0.4/googleapis.min.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/jsapi"></script>
-    <script type="text/javascript">
-        google.load("identitytoolkit", "1", {packages: ["ac"]});
-    </script>
-    <script type="text/javascript" src="../js/jquery.greentiger.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/googleapis/0.0.4/googleapis.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/jsapi"></script>
+<script type="text/javascript">
+   google.load("identitytoolkit", "1", {packages: ["ac"]});
+</script>
     <script type='text/javascript'>
-        var strUrlImg = getUrlApi("users/" + "<?php echo $strId ?>" + "?part=image");
+        #var strUrlImg = getUrlApi("users/" + "<?php echo $strId ?>" + "?part=image");
         var userData = { 
             email: "<?php echo $strEmail ?>", 
             displayName: "<?php echo $strName ?>", 
-            photoUrl: strUrlImg
+            photoUrl: "test",
         };
         #window.google.identitytoolkit.updateSavedAccount(userData);
         window.localStorage.setItem("hej", "test");
