a877082e1a390a5c6fa064c58efbca4a64d19de1
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-07 22:32:19 +0000

    user

--- js/jquery.greentiger.js
+++ js/jquery.greentiger.js
@@ -121,7 +121,7 @@ function submitFormJSON(strSelector, strUrl, strType, strAsync) {
         }
         
         function getHtmlTaskRow(id, title, name, description, type, value, config, required) {
-            var strHtml = '<article id="' + id + '"><div class="header"><span class="header">' + title +'</span> (<span class="link">?</span>)</div><div class="input">';
+            var strHtml = '<article id="' + id + '"><div class="header"><span class="header">' + title +'</span> (<span class="link" title="' + description + '">?</span>)</div><div class="input">';
             strHtml += getHtmlTaskInput(name, type, value, config, required);
             strHtml += '</div><div class="description invisible clear"><span class="description">' + description + '</span></div></article>';
             return strHtml;
