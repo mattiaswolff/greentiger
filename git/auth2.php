b4751db6fdd8305e4e20dbbdc0d5be862b54c0d4
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-07 01:00:24 +0000

    user

--- git/auth2.php
+++ git/auth2.php
@@ -14,7 +14,6 @@ if (isset($_SESSION['userId'])) {
     $objUser->upsert();
     $strRedirectUri = "http://www.zowgle.com/client/dashboard.php?userId=" . $strId . "#access_token=" . $arrNewAccessToken['token'] . "|" . $strId . "&token_type=example&expires_in=4301";  
     echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script><script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/googleapis/0.0.4/googleapis.min.js"></script><script type="text/javascript" src="https://ajax.googleapis.com/jsapi"></script><script type="text/javascript">google.load("identitytoolkit", "1", {packages: ["ac"]});</script><script type="text/javascript" src="../js/jquery.greentiger.js"></script><script type="text/javascript">var strUrlImg = getUrlApi("users/" + "'. $strId .'" + "?part=image");var userData = {email: "'. $strEmail . '",displayName: "' . $strName . '", photoUrl: strUrlImg,};window.google.identitytoolkit.updateSavedAccount(userData);window.localStorage.setItem("test", "nisse");</script>';
-    die();
     header('Location: ' . $strRedirectUri);
 }
 else { header('Location: http://www.dn.se');}
