b4c81a49ecc1fab7d91d4a38cda9621fa00063d0
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-14 17:37:20 +0000

    users

--- client/include/index_content.php
+++ client/include/index_content.php
@@ -1,4 +1,16 @@
+<div class="firstpage-head">
+<div class="firstpage-head-text">
+<h1>Ta tillvara på det fulla värdet av Ert företags kommunikation. Har ditt företag råd att inte haka på?!</h1>
+<h2>Zowgle är en digital hub där ditt företags kommunikation står i centrum. Vårt mål är att hjälpa Er att strukturera och effektivisera er kommunikation för att nå er fulla potential!</h2>
+</div>
+<div class="firstpage-head-media">
+</div>
+</div>
+<div class="firstpagehead-functionality">
+</div>
+<div class="firstpagehead-customers">
 First 100 customers<br/>
 <a href="http://www.zowgle.com/zowgle">Zowgle</a><br/>
 <a href="http://www.zowgle.com/zowgle-developers">Zowgle Developers</a><br/>
 <a href="http://www.zowgle.com/medius">Medius</a>
+</div>
