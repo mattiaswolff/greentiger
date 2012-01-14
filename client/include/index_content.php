4c691099044d52b241b54b668740ff2466efeaa0
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-14 16:06:06 +0000

    user

--- client/include/index_content.php
+++ client/include/index_content.php
@@ -1,4 +1,4 @@
 First 100 customers
-<a href="http://www.zowgle.com/zowgle">Zowgle</a>
-<a href="http://www.zowgle.com/zowgle-developers">Zowgle Developers</a>
+<a href="http://www.zowgle.com/zowgle">Zowgle</a></br>
+<a href="http://www.zowgle.com/zowgle-developers">Zowgle Developers</a></br>
 <a href="http://www.zowgle.com/medius">Medius</a>
