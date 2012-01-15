cca29c2df5a56943356c2c0cb41e8f8c3c0ec435
Mattias Wolff <mattias.wolff@gmail.com>
2012-01-15 10:12:30 +0000

    users

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
                     <h1>Ta tillvara på det fulla värdet av Ert företags kommunikation. Har ditt företag råd att inte haka på?!</h1>
                     <h2>Zowgle är en digital hub där ditt företags kommunikation står i centrum. Vårt mål är att hjälpa Er att strukturera och effektivisera er kommunikation för att nå er fulla potential!</h2>
                 </div>
-                <div class="clear" />
+                <div class="clear"></div>
             </div>
             <div class="firstpagehead-action">
                 <span class="button cta rounded blue">Register now for 1 month free trial!</span>
