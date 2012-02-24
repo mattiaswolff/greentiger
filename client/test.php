8cee46e071e8f51fadb29c958e83c078a0e407fe
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-24 22:04:11 +0000

    'user'

--- client/test.php
+++ client/test.php
@@ -117,22 +117,10 @@
         <div class="span2">
           <div class="well sidebar-nav">
             <ul class="nav nav-list">
-              <li class="nav-header">Sidebar</li>
-              <li class="active"><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li class="nav-header">Sidebar</li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li class="nav-header">Sidebar</li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
+              <li class="nav-header">Definitions</li>
+              <li class="active"><a href="#">Praise</a></li>
+              <li><a href="#">Problem</a></li>
+              <li><a href="#">Other</a></li>
             </ul>
           </div><!--/.well -->
         </div>
