73cc4ce6d3caec92ff3f5d9c603be16c32bdbb81
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-24 21:23:20 +0000

    'user'

--- client/test.php
+++ client/test.php
@@ -45,9 +45,9 @@
           <a class="brand" href="#">Project name</a>
           <div class="nav-collapse">
             <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li class="active"><a href="#profile">Profile</a></li>
+              <li><a href="#profile">Definitions</a></li>
+              <li><a href="#plugins">Plugins</a></li>
             </ul>
             <p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>
           </div><!--/.nav-collapse -->
