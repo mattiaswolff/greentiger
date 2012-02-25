119b09946a269943fb43712d416425f2656e6c5b
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-25 12:41:26 +0000

    'bootstrap'

--- client/test.php
+++ client/test.php
@@ -75,13 +75,11 @@
           <div class="well">
             <form class="form-horizontal">
               <div class="form-actions">
-                  <button class="btn" data-toggle="button">Fråga</button>
-              <button class="btn" data-toggle="button">Förslag</button>
-              <button class="btn" data-toggle="button">Beröm</button>
-              <button class="btn" data-toggle="button">Förslag</button>
+                  <button class="btn-info" data-toggle="button">Fråga</button>
+                  <button class="btn-info" data-toggle="button">Förslag</button>
+                  <button class="btn-info" data-toggle="button">Beröm</button>
+                  <button class="btn-info" data-toggle="button">Förslag</button>
               </div>
-              <br/>
-              <br/>
               <fieldset>
                 <div class="control-group">
                   <label class="control-label" for="input01">Text input</label>
