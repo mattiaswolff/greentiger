94e913d4567b4d42ed4250bea51bed5d05ca1c8b
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-25 16:02:30 +0000

    'bootstrap'

--- client/test.php
+++ client/test.php
@@ -179,14 +179,13 @@
               <h3>Modal Heading</h3>
             </div>
             <div class="modal-body">
-              <h4>Text in a modal</h4>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem.</p>
-
-              <h4>Popover in a modal</h4>
-              <p>This <a href="#" class="btn popover-test" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">button</a> should trigger a popover on hover.</p>
-
-              <h4>Tooltips in a modal</h4>
-              <p><a href="#" class="tooltip-test" data-original-title="Tooltip">This link</a> and <a href="#" class="tooltip-test" data-original-title="Tooltip">that link</a> should have tooltips on hover.</p>
+              <form class="well">
+                <label>Label name</label>
+                <input type="text" class="span3" placeholder="Type something…">
+                <span class="help-inline">Associated help text!</span>
+                <label class="checkbox"><input type="checkbox">Check me out</label>
+                <button type="submit" class="btn">Submit</button>
+            </form>
             </div>
             <div class="modal-footer">
               <a href="#" class="btn btn-primary">Save changes</a>
