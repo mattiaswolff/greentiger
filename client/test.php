83d4425c00f920f47510872dcfafc03336a75828
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-25 07:44:22 +0000

    'user'

--- client/test.php
+++ client/test.php
@@ -181,6 +181,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <script src="../js/bootstrap-transition.js"></script>
     <script src="../js/bootstrap-alert.js"></script>
     <script src="../js/bootstrap-modal.js"></script>
