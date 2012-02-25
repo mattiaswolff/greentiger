1cd68e2cc6be64bf6b77935566ed42ae2f929a81
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-25 15:56:19 +0000

    'bootstrap'

--- client/test.php
+++ client/test.php
@@ -191,19 +191,26 @@
 
     </div><!--/.fluid-container-->
     
-    <div class="modal fade" id="myModal">
-  <div class="modal-header">
-    <a class="close" data-dismiss="modal">×</a>
-    <h3>Modal header</h3>
-  </div>
-  <div class="modal-body">
-    <p>One fine body…</p>
-  </div>
-  <div class="modal-footer">
-    <a href="#" class="btn btn-primary">Save changes</a>
-    <a href="#" class="btn">Close</a>
-  </div>
-</div>
+    <div id="myModal" class="modal hide fade in" style="display: block; ">
+            <div class="modal-header">
+              <a class="close" data-dismiss="modal">×</a>
+              <h3>Modal Heading</h3>
+            </div>
+            <div class="modal-body">
+              <h4>Text in a modal</h4>
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem.</p>
+
+              <h4>Popover in a modal</h4>
+              <p>This <a href="#" class="btn popover-test" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">button</a> should trigger a popover on hover.</p>
+
+              <h4>Tooltips in a modal</h4>
+              <p><a href="#" class="tooltip-test" data-original-title="Tooltip">This link</a> and <a href="#" class="tooltip-test" data-original-title="Tooltip">that link</a> should have tooltips on hover.</p>
+            </div>
+            <div class="modal-footer">
+              <a href="#" class="btn btn-primary">Save changes</a>
+              <a href="#" class="btn" data-dismiss="modal">Close</a>
+            </div>
+          </div>
     
     <!-- Le javascript
     ================================================== -->
