d7a722a2276db9d463636d7acb42e92819b572e2
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-26 11:15:37 +0100

    images

--- classes/element.php
+++ classes/element.php
@@ -39,7 +39,7 @@ class Element {
     public function setConfig($x = null) {if ($x != null) { $this->config = $x; }} 
     
     //Get, Upsert and Delete functions
-    function get($intObjectsPerPage = 10, $intPage = 1, $arrDefinitionId = null, $arrElementId = null,) {
+    function get($intObjectsPerPage = 10, $intPage = 1, $arrDefinitionId = null, $arrElementId = null) {
         $m = new Mongo();
         $db = $m->projectcopperfield;
         $intSkip = (int)($intObjectsPerPage * ($intPage - 1));
