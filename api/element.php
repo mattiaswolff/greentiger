8f812d38c1732babd5557f97e9b2ade066dfc70a
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-26 11:07:09 +0100

    images

---
+++ api/element.php
@@ -0,0 +1,88 @@
+<?php
+require "../classes/rest.php";
+require "../classes/definition.php";
+require "../classes/user.php";
+
+$data = RestUtils::processRequest();  
+
+switch($data->getMethod()) {  
+    case 'get':
+        
+        $arrRequestVars = $data->getRequestVars();
+        $strDefinitionId = (isset($arrRequestVars['definitionId']) ? $arrRequestVars['definitionId'] : '');
+        
+        if ($strDefinitionId != '') {
+            $arrId[] = new MongoId($strDefinitionId);
+        }
+        else {
+            RestUtils::sendResponse(400, 'error', 'application/json');
+            break;
+        }
+        $arrResults = Element::get(10, 1, $arrId);
+        RestUtils::sendResponse(200, $arrResults, 'application/json');
+        break;
+    case 'post':
+        $arrRequestVars = $data->getRequestVars();
+        $strDefinitionId = (isset($arrRequestVars['definitionId']) ? $arrRequestVars['definitionId'] : '');
+        $strUserId = (isset($arrRequestVars['userId']) ? $arrRequestVars['userId'] : '');
+        
+        if ($strUserId != '') {
+            $objDefinition = new Definition();
+            $objDefinition->setId();
+            $objDefinition->setName($arrRequestVars["name"]);
+            $objDefinition->setDescription($arrRequestVars["description"]);
+            $objDefinition->setContent($arrRequestVars['content']);
+            $objDefinition->upsert();
+            $objUser = new User(new MongoId($strUserId));
+            $arrDefinitions = $objUser->getDefinitions();
+            $arrDefinition['_id'] = $objDefinition->getId();;
+            $arrDefinition['name'] = $objDefinition->getName();
+            $arrDefinition['description'] = $objDefinition->getDescription();
+            $arrDefinitions[] = $arrDefinition;
+            $objUser->setDefinitions($arrDefinitions);
+            $objUser->upsert();
+            RestUtils::sendResponse(200, (array)$objDefinition->getId(), 'application/json');
+        }
+        else {
+            $objDefinition = new Definition();
+            $objDefinition->setId();
+            $objDefinition->setName($arrRequestVars["name"]);
+            $objDefinition->setDescription($arrRequestVars["description"]);
+            $objDefinition->setContent($arrRequestVars['content']);
+            $objDefinition->upsert();
+            RestUtils::sendResponse(200, (array)$objDefinition->getId(), 'application/json');
+        }
+        break;
+    case 'put':
+        $arrRequestVars = $data->getRequestVars();
+        
+        $strDefinitionId = (isset($arrRequestVars['definitionId']) ? $arrRequestVars['definitionId'] : '');
+        $strUserId = (isset($arrRequestVars['userId']) ? $arrRequestVars['userId'] : '');
+        
+        if ($strDefinitionId != '') {
+            $objDefinition = new Definition(new MongoId($strDefinitionId));
+            $objDefinition->setName($arrRequestVars['name']);
+            $objDefinition->setDescription($arrRequestVars['description']);
+            $objDefinition->setContent($arrRequestVars['content']);
+            $objDefinition->upsert();
+            RestUtils::sendResponse(200, (array)$objDefinition->getId(), 'application/json');
+        }
+        else {
+            RestUtils::sendResponse(400);
+        }
+        break;
+    case 'delete':
+        $arrRequestVars = $data->getRequestVars();
+        $strDefinitionId = (isset($arrRequestVars['definitionId']) ? $arrRequestVars['definitionId'] : '');
+        $strUserId = (isset($arrRequestVars['userId']) ? $arrRequestVars['userId'] : '');
+        if ($strDefinitionId != '') {
+            $arrObjectId[] = new MongoId($strDefinitionId);
+            $intStatus = Definition::delete($arrObjectId);
+            RestUtils::sendResponse($intStatus, $intStatus, 'application/json');
+        }
+        else {
+            RestUtils::sendResponse(400);
+        }
+        break;
+}
+?>
---
+++ classes/element.php
@@ -0,0 +1,99 @@
+<?php
+
+class Definition {
+
+    //Properties
+    private $id;
+    private $description;
+    private $type;
+    private $config;
+  
+    //Constructor
+    public function __construct($objId = null){  
+        if ($objId != null) {
+            $m = new Mongo();
+            $db = $m->projectcopperfield;
+            $arrResults = $db->definitions->findOne(array("_id" => $objId));
+            $this->id = $arrResults['id'];
+            $this->description = $arrResults['description'];
+            $this->type = $arrResults['type'];            
+            $this->config = $arrResults['config'];
+        }
+        else {
+            $this->id = '';
+            $this->name = '';  
+            $this->description = '';  
+            $this->content = array();
+            $this->updatedDate = '';
+        }
+    }
+    
+    //Accessors
+    public function getId() { return $this->id; } 
+    public function getDescription() { return $this->description; }    
+    public function getType() { return $this->type; } 
+    public function getConfig() { return $this->config; }  
+    public function setId($x = null) {if ($x != null) { $this->id = $x; }}
+    public function setDescription($x = null) {if ($x != null) { $this->description = $x; }}
+    public function setType($x = null) {if ($x != null) {$this->type = $x; }} 
+    public function setConfig($x = null) {if ($x != null) { $this->config = $x; }} 
+    
+    //Get, Upsert and Delete functions
+    function get($intObjectsPerPage = 10, $intPage = 1, $arrDefinitionId = null, $arrElementId = null,) {
+        $m = new Mongo();
+        $db = $m->projectcopperfield;
+        $intSkip = (int)($intObjectsPerPage * ($intPage - 1));
+        $intLimit = $intObjectsPerPage;
+        $objResults = $db->definitions->findOne(array("_id" => array('$in' => $arrObjectId)));
+        //report error if not found.        
+        $arrResults['page'] = $intPage;
+        $arrResults['page_size'] = $intObjectsPerPage;        
+        if ($arrElementId != null) {
+            $arrResults['total'] = 1;
+            $arrResults['elements'][] = $objResults['elements'][$arrElementId]
+            //report error if element is not found.
+        }
+        else {
+            $arrResults['total'] = 0;
+            foreach ($objResults['elements'] as $key => $var) {
+                //This loop should needs to be limited by page_size
+                $arrResults['total'] = $arrResults['total'] + 1;
+                $arrResults['elements'][] = $var;
+            }
+        }
+        return $arrResults; 
+    }
+    
+    public function upsert($intDefinitionId) {
+        $m = new Mongo();
+        $db = $m->projectcopperfield;
+        $db->definitions->update(array('id' => $intDefinitionId,
+                                        'elements.id' => $this->getId()),
+                                array('$set' =>
+                                        array('elements.$.description' => $this->getDescription(),
+                                            'elements.$.type' => $this->getType(),
+                                            'elements.$.config' => $this->getConfig()))));
+        
+        $result =
+            $db->runCommand(array('getlasterror' => 1));
+        
+        echo $result;
+    }
+    
+    function delete($arrObjectId) {
+        $m = new Mongo();
+        $db = $m->projectcopperfield;
+        $arrQuery = array("_id" => array('$in' => $arrObjectId));
+        $arrOptions = array("safe" => true);
+        $arrResults = $db->definitions->remove($arrQuery, $arrOptions);
+        $intStatus = ($arrResults['n'] == 1 ? 200 : 400);
+        return $intStatus; 
+    }
+    
+    //Other functions
+    public function toArray() {
+        $array = get_object_vars($this);
+        return $array;
+    }
+}
+?>
