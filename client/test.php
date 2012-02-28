66049cc963c40c3385f1b0474db012a07e7edac0
Mattias Wolff <mattias.wolff@gmail.com>
2012-02-28 20:29:20 +0100

    images

--- client/test.php
+++ client/test.php
@@ -263,7 +263,7 @@
                 
             });
             $('.nav-tabs').append('<li><a href="" data-toggle="tab"><i class="icon-plus"></i></a></li>');
-            $('.tab-content').append('<div class="tab-pane active" id="1"><form class="form-horizontal"><fieldset><div class="control-group"><div class="controls"><button class="btn add-element" name="" data-toggle="modal" href="#myModal"><i class="icon-plus"></i> Add element</button></div></div><div class="form-actions"><button type="submit" class="btn btn-primary">Save changes</button> <button class="btn">Cancel</button></div></fieldset></form></div>');
+            $('.tab-content').append('<div class="tab-pane" id="1"><form class="form-horizontal"><fieldset><div class="control-group"><div class="controls"><button class="btn add-element" name="" data-toggle="modal" href="#myModal"><i class="icon-plus"></i> Add element</button></div></div><div class="form-actions"><button type="submit" class="btn btn-primary">Save changes</button> <button class="btn">Cancel</button></div></fieldset></form></div>');
         });
         
         $("#myModal").delegate("button.btn-primary", "click", function(event) {
