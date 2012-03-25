5def721c0a6413cde93955517518ebab23c54b41
Mattias Wolff <mattias.wolff@gmail.com>
2012-03-25 22:31:59 +0200

    slim

--- classes/definitions.php
+++ classes/definitions.php
@@ -43,12 +43,12 @@ class Definition {
         $m = new Mongo();
         $db = $m->projectcopperfield;
         $objResults = $db->definitions->find(array("_id" => array('$in' => $arrId)));
+        echo var_dump($objResults)
         foreach ($objResults as $key => $var) {
             $definitions[] = new Definition($var['_id'], $var['name'], $var['description'], $var['elements']);
         }
         return $definitions; 
     }
-    
     
     public function upsert() {
         $m = new Mongo();
