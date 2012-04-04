df90cf376a894d8547de4e56aaa0b90710a7f447
Mattias Wolff <mattias.wolff@gmail.com>
2012-04-04 22:09:19 +0200

    user

--- client/test2.php
+++ client/test2.php
@@ -109,7 +109,6 @@
     <script type="text/template" id="definition-template-form">
         <form class="form-horizontal">
             <fieldset>
-                <legend>Legend text</legend>
                 <div class="control-group">
                     <label class="control-label" for="input01">Text input</label>
                     <div class="controls">
